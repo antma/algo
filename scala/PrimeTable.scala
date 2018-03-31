ba367eb0b2e29657017298bf937dbfdd8369f867
antma <antma@users.noreply.github.com>
2018-03-31 03:56:21 +0300

    PrimeTable.scala: improve performance by replacing immutable BitSet by mutable BitSet

--- scala/PrimeTable.scala
+++ scala/PrimeTable.scala
@@ -1,5 +1,4 @@
-
-import scala.collection.immutable.BitSet
+import scala.collection.mutable.BitSet
 
 class PrimeTable (n: Int) { //finding primes < n
   val half = n / 2
@@ -10,11 +9,10 @@ class PrimeTable (n: Int) { //finding primes < n
       val step = 2 * i + 1
       val start = 2 * i * (i + 1)
       val k = half / step
-      b ++ (start to (start + k * step) by step)
+      b ++= (start to (start + k * step) by step)
     }
   }
   //c.contains (k) if 2 * k + 1 is composite number
   val c = (1 to m).foldLeft (BitSet.empty + 0) (f)
   def primes () = if (n <= 2) List.empty else 2 +: (1 until half).filterNot (i => c.contains (i)).map (i => 2 * i + 1).toList
 }
-
