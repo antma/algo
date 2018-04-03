5a6016684ee33ced2158fc0b89488d8a4e12cd14
antma <antma@users.noreply.github.com>
2018-04-03 17:08:30 +0300

    DisjointSet.scala

---
+++ scala/DisjointSet.scala
@@ -0,0 +1,27 @@
+class DisjointSet (n: Int) {
+  val p = (0 until n).toArray
+  val h = Array.ofDim[Int] (n)
+  def find_set (x: Int): Int = {
+    if (p(x) == x) x
+    else {
+      p(x) = find_set (p(x))
+      p(x)
+    }
+  }
+  def merge (x: Int, y: Int): Boolean = {
+    val i = find_set (x)
+    val j = find_set (y)
+    if (i == j) false
+    else {
+      if (h(i) < h(j)) {
+        p(i) = j
+      } else if (h(i) > h(j)) {
+        p(j) = i
+      } else {
+        p(i) = j
+        h(j) += 1
+      }
+      true
+    }
+  }
+}
