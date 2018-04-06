4c56d6e9e931bd5d5b6f31bd5311bc4a1f931904
antma <antma@users.noreply.github.com>
2018-04-06 21:48:52 +0300

    GF.scala: powmod function

---
+++ scala/GF.scala
@@ -0,0 +1,15 @@
+object GF {
+  def powmod (x: Int, k: Int, q: Int) = {
+    var a = 1
+    var b = x
+    var p = k
+    while (p > 0) {
+      if ((p & 1) != 0) {
+        a = ((a.toLong * b.toLong) % q).toInt
+      }
+      b = ((b.toLong * b.toLong) % q).toInt
+      p = (p >> 1)
+    }
+    a
+  }
+}
