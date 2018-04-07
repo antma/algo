9d2b5bf932be6c636e5056f699756d3e4b552049
antma <antma@users.noreply.github.com>
2018-04-07 22:29:13 +0300

    SegmentTreeSliceUpdate.d: operations - increment on segment, extract element

--- dlang/segment_tree.d
+++ dlang/segment_tree.d
@@ -110,6 +110,55 @@ struct LongestZeroSegment {
   }
 }
 
+class SegmentTreeSliceUpdate(T = int) {
+  private:
+  T [] t;
+  int n;
+  final void build (const T [] a, int v, int l, int r) {
+    if (l == r) {
+      t[v] = a[l];
+    } else {
+      immutable m = (l + r) >> 1;
+      build (a, v << 1, l, m);
+      build (a, (v << 1) + 1, m + 1, r);
+    }
+  }
+  final private void update (int v, int l, int r, int a, int b, T value) {
+    if (a <= b) {
+      if (l == a && r == b) {
+        t[v] += value;
+      } else {
+        immutable m = (l + r) >> 1;
+        immutable w = v << 1;
+        update (w, l, m, a, min (m, b), value);
+        update (w + 1, m + 1, r, max (m + 1, a), b, value);
+      }
+    }
+  }
+  final private T get (int v, size_t l, size_t r, size_t index) const {
+    if (l == r) {
+      return t[v];
+    } else {
+      immutable m = (l + r) >> 1;
+      T x = t[v];
+      x += (index <= m) ? get ((v << 1), l, m, index) : get ((v << 1) + 1, m + 1, r, index);
+      return x;
+    }
+  }
+  public:
+  final void update (int a, int b, T value) {
+    update (1, 0, n - 1, a, b, value);
+  }
+  final inout(T) opIndex (size_t index) inout {
+    return get (1, 0, n - 1, index);
+  }
+  this (const T [] a) {
+    n = a.length.to!(int);
+    t = new T[4 * n];
+    build (a, 1, 0, n - 1);
+  }
+}
+
 unittest {
   writeln ("Testing segment_tree.d ...");
   auto st = new SegmentTree!long ([1L, 2L]);
