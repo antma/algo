2541e378c3492db0f0e461e6b1b17a5a8b7418e8
antma <antma@users.noreply.github.com>
2018-04-16 18:41:09 +0300

    IntM.scala: modulo 10^9+7  (+, -, *) operations

---
+++ scala/IntM.scala
@@ -0,0 +1,14 @@
+object IntM {
+  val q = 1000000007
+  def fromInt (v: Int) = { val x = v % q; new IntM (if (x < 0) x + q else x) }
+}
+
+class IntM private (private val v: Int) {
+  def + (rhs: IntM) = new IntM ((v + rhs.v) % IntM.q)
+  def - (rhs: IntM) = new IntM ((v - rhs.v + IntM.q) % IntM.q)
+  def * (rhs: IntM) = new IntM (((v.toLong * rhs.v.toLong) % IntM.q).toInt)
+  def + (rhs: Int): IntM = this + (IntM.fromInt (rhs))
+  def - (rhs: Int): IntM = this - (IntM.fromInt (rhs))
+  def * (rhs: Int): IntM = this * (IntM.fromInt (rhs))
+  override def toString = v.toString
+}
