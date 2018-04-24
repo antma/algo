55b6bd9ba65df39b4dca9144459b9cec124bb538
antma <antma@users.noreply.github.com>
2018-04-24 18:58:19 +0300

    string_functions.d: Prefix function and Z function

---
+++ dlang/string_functions.d
@@ -0,0 +1,53 @@
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.math;
+import std.range;
+import std.stdio;
+import std.string;
+
+int[] computePrefixFunction (string s) {
+  immutable n = s.length;
+  auto p = new int[n];
+  p[0] = 0;
+  int k = 0;
+  foreach (q; 1 .. n) {
+    while (k > 0 && s[k] != s[q]) {
+      k = p[k-1];
+    }
+    if (s[k] == s[q]) {
+      k++;
+    }
+    p[q] = k;
+  }
+  return p;
+}
+
+int[] computeZFunction (string s) {
+  immutable n = s.length;
+  auto z = new int[n];
+  size_t l, r;
+  foreach (i; 1 .. n) {
+    debug stderr.writefln ("i = %d, l = %d, r = %d", i, l, r);
+    int k;
+    if (i <= r) {
+      k = (min (r - i + 1, z[i - l]));
+    }
+    while (i + k < n && s[k] == s[i+k]) {
+      ++k;
+    }
+    immutable j = i + k - 1;
+    if (j > r) {
+      l = i;
+      r = j;
+    }
+    z[i] = k;
+  }
+  return z;
+}
+
+unittest {
+  import std.stdio;
+  writeln ("Testing string_functions.d ...");
+  assert (equal (computePrefixFunction ("ababababca"), [0, 0, 1, 2, 3, 4, 5, 6, 0, 1]));
+}
