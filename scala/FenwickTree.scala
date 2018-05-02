1af69e9acef614468723e230963550e7a1a4821e
antma <antma@users.noreply.github.com>
2018-05-03 01:53:20 +0300

    FenwickTree.scala: FenwickTree2D

--- scala/FenwickTree.scala
+++ scala/FenwickTree.scala
@@ -23,13 +23,29 @@ class FenwickTree[T : reflect.ClassTag] (n: Int, op: (T, T) => T, default_value:
   val a = Array.fill (n) (default_value)
   def add (x: Int, v: T) {
     for (i <- new FenwickTree.IterA (x, n)) {
-      a(i) = op (a(i), v)  
+      a(i) = op (a(i), v)
     }
   }
   def reduce (x: Int): T = {
     var r = default_value
     for (i <- new FenwickTree.IterR (x)) {
       r = op (r, a(i))
+    }
+    r
+  }
+}
+
+class FenwickTree2D[T : reflect.ClassTag] (dx: Int, dy:Int, op: (T, T) => T, default_value: T) {
+  val a = Array.fill (dx, dy) (default_value)
+  def add (x: Int, y: Int, v: T) {
+    for (i <- new FenwickTree.IterA (x, dx); j <- new FenwickTree.IterA (y, dy)) {
+      a(i)(j) = op (a(i)(j), v)
+    }
+  }
+  def reduce (x: Int, y: Int): T = {
+    var r = default_value
+    for (i <- new FenwickTree.IterR (x); j <- new FenwickTree.IterR (y)) {
+      r = op (r, a(i)(j))
     }
     r
   }
