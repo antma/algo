de2f59d93efb89c350df480ef07f11df62677a32
antma <antma@users.noreply.github.com>
2018-06-18 17:56:35 +0300

    MaxFlow.cpp: Push relabel maximum flow algorithm O(V^3) (replace graph flow matrix by edge list)

--- c++/MaxFlow.cpp
+++ c++/MaxFlow.cpp
@@ -6,47 +6,49 @@
 
 using namespace std;
 
+using Cost = int;
+using Excess = int;
+const Excess MAX_EXCESS = INT_MAX;
+
+struct Edge {
+  Cost f;
+  const Cost c;
+  const int v, e;
+  Edge (const Cost _c, int _v, int _e) :
+    f (0), c (_c), v (_v), e (_e) {
+  }
+};
+
 class PushRelabelMaxFlowGraph {
   private:
-  vector<vector<pair<int, int> > > edges;
-  vector<int> f;
+  vector<vector<Edge> > edges;
+  vector<vector<Edge>::iterator> current;
   vector<int> h;
-  vector<int64_t> e;
-  vector<vector<pair<int, int>>::const_iterator> current;
+  vector<Excess> e;
   vector<int> next, prev;
-  int n;
-  void buildEdges () {
-    for (int i = 0; i < n; ++i) for (int j = i + 1; j < n; ++j) {
-      if (f[i * n + j] > 0 || f[j * n + i] > 0) {
-        edges[i].emplace_back (j, f[i * n + j]);
-        edges[j].emplace_back (i, f[j * n + i]);
-        f[i * n + j] = f[j * n + i] = 0;
-      }
-    }
-  }
+  const int n;
   void initPreflow () {
     h[0] = n;
-    for (const auto &p : edges[0]) {
-      const int i = p.first;
-      e[i] = f[i] = p.second;
-      f[i * n] = -p.second;
+    for (auto &p : edges[0]) {
+      const int i = p.v;
+      e[i] = p.f = p.c;
+      edges[p.v][p.e].f = -p.c;
     }
   }
-  void push (int i, int j, int cij) {
-    int ij = i * n + j;
-    int d = cij - f[ij];
+  void push (int i, Edge &edge) {
+    const int j = edge.v;
+    Cost d = edge.c - edge.f;
     if (d > e[i]) d = e[i];
-    f[ij] += d;
-    f[j * n + i] = -f[ij];
+    edge.f += d;
+    edges[j][edge.e].f = -edge.f;
     e[i] -= d;
     e[j] += d;
   }
   void lift (int i) {
     int m = INT_MAX;
     for (const auto &p : edges[i]) {
-      const int j = p.first;
-      if (f[i * n + j] < p.second) {
-        if (m > h[j]) m = h[j];
+      if (p.f < p.c && m > h[p.v]) {
+        m = h[p.v];
       }
     }
     assert (m < INT_MAX);
@@ -55,14 +57,16 @@ class PushRelabelMaxFlowGraph {
   void discharge (int i) {
     while (e[i] > 0) {
       auto l = current[i];
-      if (l == edges[i].cend ()) {
+      if (l == edges[i].end ()) {
         lift (i);
-        current[i] = edges[i].cbegin ();
+        current[i] = edges[i].begin ();
       } else {
-        const auto &p = *(current[i]);
-        const int j = p.first;
-        if (f[i * n + j] < p.second && h[i] == h[j] + 1) push (i, j, p.second);
-        else ++(current[i]);
+        auto &p = *(current[i]);
+        if (p.f < p.c && h[i] == h[p.v] + 1) {
+          push (i, p);
+        } else {
+          ++(current[i]);
+        }
       }
     }
   }
@@ -72,28 +76,28 @@ class PushRelabelMaxFlowGraph {
   }
   public:
   inline void addEdge (int i, int j, int w) {
-    f[i * n + j] = w;
+    const int ei = edges[i].size (), ej = edges[j].size ();
+    edges[i].emplace_back (w, j, ej);
+    edges[j].emplace_back (0, i, ei);
   }
   //0 - sink, (n - 1) - target
   PushRelabelMaxFlowGraph (int _n) :
     edges (_n),
-    f (_n * _n, 0),
+    current (_n),
     h (_n, 0),
     e (_n, 0),
-    current (_n),
     next (_n), prev (_n),
     n (_n)
   {
 
   }
-  int64_t maxFlow () {
+  Excess maxFlow () {
     int i;
-    buildEdges ();
     initPreflow ();
     for (i = 1; i < n - 1; ++i) {
       next[i] = i + 1;
       prev[i] = i - 1;
-      current[i] = edges[i].cbegin ();
+      current[i] = edges[i].begin ();
     }
     next[0] = 1;
     addLink (n - 2, 0);
@@ -109,9 +113,6 @@ class PushRelabelMaxFlowGraph {
       }
       i = next[i];
     }
-    int64_t res = 0;
-    for (i = 1; i < n; ++i) res += f[i];
-    return res;
+    return e[n-1];
   }
 };
-
