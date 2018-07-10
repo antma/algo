1e24d90d6e46ff7a201eef1c666c31bc14752333
antma <antma@users.noreply.github.com>
2018-07-10 23:21:23 +0300

    treap.d: [optimization] replace class by pointers to struct for treap nodes

--- dlang/treap.d
+++ dlang/treap.d
@@ -4,54 +4,42 @@ import std.random;
 //flags: +2 - has size
 class Treap(Key, Value, int flags = 3) {
   private:
-  Node root;
-  static class Node {
-    Node left, right;
-    immutable Key x;
-    static if (flags & 1) immutable Value value;
-    immutable int y;
+  static struct Node {
+    Node *left;
+    Node *right;
+    Key x;
+    static if (flags & 1) Value value;
+    int y;
     static if (flags & 2) int sz;
-    static if (flags & 1) {
-      this (Key _x, int _y, Value _value) {
-        x = _x;
-        y = _y;
-        value = _value;
-        static if (flags & 2) sz = 1;
-      }
-    } else {
-      this (Key _x, int _y) {
-        x = _x;
-        y = _y;
-        static if (flags & 2) sz = 1;
-      }
-    }
   }
+  static Node free_nodes;
+  static this () { free_nodes.left = free_nodes.right = &free_nodes; }
+  static Node *newNode () {
+    Node *p = free_nodes.right;
+    if (p == &free_nodes) {
+      p = new Node ();
+    } else {
+      p.left.right = p.right; p.right.left = p.left;
+    }
+    static if (flags & 2) p.sz = 1;
+    return p;
+  }
+
+  Node *root;
   static if (flags & 2) {
-    static int getSize (const Node t) {
+    static int _size (const Node *t) {
       return t ? t.sz : 0;
     }
-    static void _relax (Node t) {
+    static void _relax (Node *t) {
       t.sz = 1;
       if (t.left) t.sz += t.left.sz;
       if (t.right) t.sz += t.right.sz;
     }
-    static size_t _countLess (Node t, Key x) {
-      size_t s;
+    static Node *_kthNode (Node *t, size_t k) {
+      if (k >= _size (t)) return null;
       while (t) {
-        if (t.x < x) {
-          s += 1 + getSize (t.left);
-          t = t.right;
-        } else {
-          t = t.left;
-        }
-      }
-      return s;
-    }
-    static Node _kthNode (Node t, size_t k) {
-      if (k >= getSize (t)) return null;
-      while (t) {
-        immutable ls = getSize (t.left);
-        if (k >= ls + 1) {
+        immutable ls = _size (t.left);
+        if (k > ls) {
           k -= ls + 1;
           t = t.right;
         } else if (k < ls) {
@@ -63,15 +51,14 @@ class Treap(Key, Value, int flags = 3) {
       assert (false);
     }
   }
-
-  static Node _find (Node t, Key x) {
+  static Node *_find (Node *t, Key x) {
     while (t && t.x != x) {
       t = (x < t.x) ? t.left : t.right;
     }
     return t;
   }
 
-  static void _split (Node t, Key x, ref Node l, ref Node r) {
+  static void _split (Node *t, Key x, ref Node* l, ref Node* r) {
     if (!t) {
       l = r = null;
       return;
@@ -86,8 +73,9 @@ class Treap(Key, Value, int flags = 3) {
     static if (flags & 2) _relax (t);
   }
 
-  static Node _insert (Node t, Node p) {
+  static Node *_insert (Node *t, Node *p) {
     if (!t) {
+      p.left = p.right = null;
       return p;
     }
     if (t.y >= p.y) {
@@ -96,7 +84,7 @@ class Treap(Key, Value, int flags = 3) {
       } else {
         t.right = _insert (t.right, p);
       }
-      static if (flags & 2) _relax (t);
+      static if (flags & 2) ++t.sz;
       return t;
     }
     _split (t, p.x, p.left, p.right);
@@ -104,7 +92,7 @@ class Treap(Key, Value, int flags = 3) {
     return p;
   }
 
-  static Node _merge (Node l, Node r) {
+  static Node *_merge (Node *l, Node *r) {
     if (!l) {
       return r;
     } else if (!r) {
@@ -120,54 +108,66 @@ class Treap(Key, Value, int flags = 3) {
     }
   }
 
-  static Node _remove (Node t, Key x, ref Node parent, ref bool right_path) in {
-    assert (t);
-  } body {
-    if (t.x == x) {
-      return t;
+  static Node *_remove (ref Node **w, Key x, ) {
+    static if (flags & 2) {
+      Node*[128] path = void;
+      int m = -1;
     }
-    parent = t;
-    static if (flags & 2) --t.sz;
-    if (x < t.x) {
-      right_path = false;
-      return _remove (t.left, x, parent, right_path);
+    Node **p = w;
+    Node *t = *p;
+    while (t) {
+      static if (flags & 2) {
+        if (t.x == x) {
+          foreach (i; 0 .. m + 1) {
+            --path[i].sz;
+          }
+          w = p;
+          return t;
+        }
+        path[++m] = t;
+      } else {
+        if (t.x == x) {
+          w = p;
+          return t;
+        }
+      }
+      p = (x < t.x) ? &(t.left) : &(t.right);
+      t = *p;
     }
-    right_path = true;
-    return _remove (t.right, x, parent, right_path);
+    return null;
   }
 
   public:
 
   static if (flags & 1) {
     final void insert (Key key, Value value) {
-      root = _insert (root, new Node (key, uniform (int.min, int.max), value));
+      Node *p = newNode ();
+      p.x = key;
+      p.y = uniform (int.min, int.max);
+      p.value = value;
+      root = _insert (root, p);
     }
   } else {
     final void insert (Key key) {
-      root = _insert (root, new Node (key, uniform (int.min, int.max)));
+      Node *p = newNode ();
+      p.x = key;
+      p.y = uniform (int.min, int.max);
+      root = _insert (root, p);
     }
   }
 
-  final void remove (Key x) {
-    Node parent;
-    bool right_path;
-    auto t = _remove (root, x, parent, right_path);
-    Node p = _merge (t.left, t.right);
-    if (parent) {
-      right_path ? parent.right : parent.left = p;
-    } else {
-      root = p;
-    }
+  final bool remove (Key x) {
+    Node **w = &root;
+    auto t = _remove (w, x);
+    if (!t) return false;
+    *w =  _merge (t.left, t.right);
+    Node *u = free_nodes.left, v = &free_nodes;
+    u.right = t; t.left = u;
+    t.right = v; v.left = t;
+    return true;
   }
-  static if (flags & 2) {
-    final size_t countLess (Key x) { return _countLess (root, x); }
-    final void kthKey (size_t k, ref Key x) {
-      Node p = _kthNode (root, k);
-      if (p) {
-        x = p.x;
-      }
-    }
-  }
+
+  final bool contains (Key x) { return _find (root, x) !is null; }
 }
 
 class ImplicitKeyTreap(Value) {
@@ -300,4 +300,14 @@ unittest {
   t.insert (1, 2);
   auto t2 = new Treap!(int, long, 2);
   t2.insert (1);
+  assert (t2.contains (1));
+  assert (t2.remove (1));
+  assert (!t2.contains (1));
+
+  auto t0 = new Treap!(int, int, 0);
+  t0.insert (1);
+  assert (t0.contains (1));
+  assert (t0.remove (1));
+  assert (!t0.contains (1));
+
 }
