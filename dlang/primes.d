0727240038bc671263b467b60f777ee15e394420
antma <antma@users.noreply.github.com>
2018-07-18 01:08:59 +0300

    primes.d: [optimization] primes array generation (iterating BitSet using bitsSet method)

--- dlang/primes.d
+++ dlang/primes.d
@@ -1,13 +1,4 @@
 import std.algorithm, std.conv, std.math, std.range;
-/*
-import std.algorithm;
-import std.array;
-import std.conv;
-import std.math;
-import std.stdio;
-import std.string;
-*/
-
 import std.bitmanip;
 
 class PrimeTable {
@@ -18,26 +9,23 @@ class PrimeTable {
   bool isPrime (size_t i) const pure nothrow @nogc {
     return (i & 1) ? !a[i>>>1] : (i == 2);
   }
-  size_t [] primes () const pure nothrow {
-    auto p = new size_t[0];
+  int[] primes () const pure {
+    int[] p;
     if (n > 2) {
       p ~= 2;
-      foreach (i; iota (3, n, 2)) {
-        if (!a[i>>>1]) {
-          p ~= i;
-        }
-      }
+      p ~= (~a).bitsSet.map! (i => (2 * i + 1).to!int).array;
     }
     return p;
   }
-  this (size_t n_) pure {
-    n = n_;
+  this (size_t _n) pure {
+    n = _n;
     auto m = n >> 1;
     a.length = max (1, m);
     a[0] = true;
     foreach (i; 1 .. ceil((sqrt (n.to!(double)) - 1.0)).to!(size_t)) {
       if (!a[i]) {
-        for (size_t j = 2 * i * (i + 1); j < m; j += (i << 1) + 1) {
+        immutable k = (i << 1) + 1;
+        for (size_t j = 2 * i * (i + 1); j < m; j += k) {
           a[j] = true;
         }
       }
