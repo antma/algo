fa0086b1f721a9c19a95ee1fd17c012bdbc20330
antma <antma@users.noreply.github.com>
2018-07-19 15:20:37 +0300

    disjoint_set.d: add container based on DSU for integers from 0 to n with slice iteration and remove operation

--- dlang/disjoint_set.d
+++ dlang/disjoint_set.d
@@ -1,4 +1,4 @@
-import std.algorithm, std.range;
+import std.algorithm, std.conv, std.range;
 
 class DisjointSet {
   private:
@@ -38,4 +38,59 @@ class DisjointSet {
     return c.reduce! (max);
   }
 }
-
+class DisjointSetList {
+  private:
+  int[] p, h, prev, next, maxv;
+  int n;
+  struct Range {
+    int[] _next;
+    int begin;
+    immutable int end;
+    @property bool empty () const pure nothrow @nogc { return begin >= end; }
+    @property int front () const pure nothrow @nogc { return begin; }
+    void popFront () pure nothrow @nogc { begin = _next[begin]; }
+  }
+  final int find_set (int x) pure nothrow @nogc {
+    if (p[x] == x) {
+      return x;
+    }
+    return p[x] = find_set (p[x]);
+  }
+  final void join (int x, int y) pure nothrow @nogc {
+    x = find_set (x);
+    y = find_set (y);
+    maxv[x] = maxv[y];
+    if (h[x] < h[y]) {
+      p[x] = y;
+    } else if (h[x] > h[y]) {
+      p[y] = x;
+    } else {
+      p[x] = y;
+      ++h[y];
+    }
+  }
+  public:
+  this (int _m) pure nothrow {
+    n = _m + 1;
+    p = iota (0, n).array;
+    h = new int[n];
+    prev = new int[n];
+    next = new int[n];
+    maxv = p.dup;
+    foreach (i; 0 .. _m) {
+      next[i] = i + 1;
+      prev[i+1] = i;
+    }
+    prev[0] = _m;
+    next[_m] = 0;
+  }
+  final void removeKey (int k) pure nothrow @nogc {
+    join (k, next[k]);
+    int u = prev[k], v = next[k];
+    next[u] = v;
+    prev[v] = u;
+  }
+  Range opSlice (size_t begin, size_t end) pure {
+    return Range (next, maxv[find_set (begin.to!int)], end.to!int);
+  }
+};
