antma/algo

commit 4e421cdc6b353807537c8d543455f1f07bced11b

treap.d: tiny improvemnent in ImplicitKeyTreap.persistent_split

dlang/treap.d
@@ -425,14 +425,14 @@ class ImplicitKeyTreap(Value, alias Extra="", alias relax_op="", alias push_op="
       int ls;
       if (t.left) ls = t.left.sz;
       Node *q = &nodes[cur++];
-      q.left = t.left;
-      q.right = t.right;
       q.value = t.value;
       q.y = t.y;
       if (key <= ls) {
+        q.right = t.right;
         persistent_split (q.left, l, q.left, key);
         r = q;
       } else {
+        q.left = t.left;
         persistent_split (q.right, q.right, r, key - ls - 1);
         l = q;
       }

commit c3f716b94e6ae4f5ce8631399cab7b484aca4df0

treap.d: fix crush bug in persistent split introduced in 4e421cdc6b353807537c8d543455f1f07bced11b commit

dlang/treap.d
@@ -443,11 +443,11 @@ class ImplicitKeyTreap(Value, Extra=void, alias relax_op="", alias push_op="", a
       q.y = t.y;
       if (key <= ls) {
         q.right = t.right;
-        persistent_split (q.left, l, q.left, key);
+        persistent_split (t.left, l, q.left, key);
         r = q;
       } else {
         q.left = t.left;
-        persistent_split (q.right, q.right, r, key - ls - 1);
+        persistent_split (t.right, q.right, r, key - ls - 1);
         l = q;
       }
       _relax (q);
@@ -457,8 +457,7 @@ class ImplicitKeyTreap(Value, Extra=void, alias relax_op="", alias push_op="", a
       Node *t1, t2, t3;
       persistent_split (root, t1, t2, l);
       persistent_split (t2, t2, t3, r - l);
-      auto v = t2.extra;
-      return v;
+      return t2.extra;
     }
   }
 