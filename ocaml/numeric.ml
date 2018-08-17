11f3db79d4968c5784c7c594cba22a88eb08d706
antma <antma@users.noreply.github.com>
2018-08-17 15:44:26 +0300

    numeric.ml: gcd and pow for Int64

---
+++ ocaml/numeric.ml
@@ -0,0 +1,16 @@
+
+let gcd64 a b =
+  let rec loop a b =
+    if (Int64.compare b Int64.zero) = 0 then a
+    else loop b (Int64.rem a b)
+  in
+  if (Int64.compare a b) < 0 then loop b a else loop a b
+;;
+
+let pow64 a p =
+  let rec loop a b p =
+    if p > 0 then loop (if (p land 1) = 0 then a else Int64.mul a b) (Int64.mul b b) (p lsr 1)
+    else a
+  in
+  loop Int64.one a p
+;;
