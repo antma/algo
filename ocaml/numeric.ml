9e293222d15310f17b0b2148be9d6a947b9175ad
antma <antma@users.noreply.github.com>
2018-09-05 19:53:03 +0300

    numeric.ml: add powmod

--- ocaml/numeric.ml
+++ ocaml/numeric.ml
@@ -22,3 +22,13 @@ let pow64 a p =
   in
   loop Int64.one a p
 ;;
+
+let predmod x q = pred (if x > 0 then x else x + q);;
+let mulmod x y q = Int64.to_int (Int64.rem (Int64.mul (Int64.of_int x) (Int64.of_int y)) (Int64.of_int q));;
+let powmod a p q =
+  let rec loop a b p =
+    if p > 0 then loop (if (p land 1) = 0 then a else mulmod a b q) (mulmod b b q) (p lsr 1)
+    else a
+  in
+  loop 1 a p
+;;
