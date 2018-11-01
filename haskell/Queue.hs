1f225c708f26b3373524edb58a3b6e45f3e4f3d5
antma <antma@users.noreply.github.com>
2018-11-01 22:35:41 +0300

    simple queue implementation

---
+++ haskell/Queue.hs
@@ -0,0 +1,9 @@
+queueEmpty :: Queue a
+queueEmpty = ([], [])
+queuePush :: Queue a -> a -> Queue a
+queuePush (l, r) x = (l, x : r)
+queuePop :: Queue a -> (a, Queue a)
+queuePop ( (x:xs), r) =  (x, (xs, r))
+queuePop ( [], r)
+  | null r = undefined
+  | otherwise = queuePop (reverse r, [])
