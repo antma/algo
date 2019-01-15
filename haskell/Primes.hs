5c6f506be8baa4d52db957572698cb405ae57603
antma <antma@users.noreply.github.com>
2019-01-15 17:29:47 +0300

    haskell: primes sieve

---
+++ haskell/Primes.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE FlexibleContexts #-}
+module Primes (
+  primeArray,
+  isprime
+) where
+
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.Unboxed
+import Data.Bits
+
+primeArray :: Int -> UArray Int Bool
+primeArray n = runSTUArray $ do
+  let m = max 1 (shiftR n 1)
+      dhm :: Double
+      dhm = sqrt $ fromIntegral $ pred n
+      hm = ceiling dhm
+  a <- newArray (0, m) False :: ST s (STUArray s Int Bool)
+  writeArray a 0 True
+  forM_ [1 .. pred hm] $ \p -> do
+    l <- readArray a p
+    when (not l) $ do
+      let k = succ $ 2 * p
+          loop j = when (j < m) $ do
+                     writeArray a j True
+                     loop (j + k)
+      loop (2 * p * (succ p))
+  return a
+
+isprime p x = if even x then x == 2 else not (p ! (shiftR x 1))
+
