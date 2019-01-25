ca82a026c074f3b101cc3f472075d96d3cd02301
antma <antma@users.noreply.github.com>
2019-01-25 18:18:00 +0300

    haskell/Primes.hs: trivial factorization

--- haskell/Primes.hs
+++ haskell/Primes.hs
@@ -2,7 +2,8 @@
 module Primes (
   primeArray,
   isprime,
-  primes
+  primes,
+  factors, factorization
 ) where
 
 import Control.Monad
@@ -10,6 +11,7 @@ import Control.Monad.ST
 import Data.Array.ST
 import Data.Array.Unboxed
 import Data.Bits
+import Data.List
 
 primeArray :: Int -> UArray Int Bool
 primeArray n = runSTUArray $ do
@@ -34,3 +36,13 @@ isprime p x = if even x then x == 2 else not (p ! (shiftR x 1))
 primes n = 2 : (filter ( not . (pa ! ) . (`shiftR` 1))) [3, 5 .. n]
   where
     pa = primeArray n
+
+factors p n = loop n p []
+  where
+    loop 1 _ r = r
+    loop m l@(x:xs) r
+      | x * x > m = m : r
+      | (mod m x) == 0 = loop (div m x) l (x:r)
+      | otherwise = loop m xs r
+
+factorization p n = map (\l -> (head l, length l)) $ group $ factors p n
