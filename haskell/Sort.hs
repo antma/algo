4a3b144d5f0356fefb8683b53feb805ffa1b7f7b
antma <antma@users.noreply.github.com>
2019-02-17 23:10:43 +0300

    randomized qsort

--- haskell/Sort.hs
+++ haskell/Sort.hs
@@ -1,49 +1,44 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Safe, FlexibleContexts #-}
 module Sort (
   qsort, lowerbound
 ) where
 
-import Text.Printf
 import Data.Array
-import Data.Array.ST
+import Data.Array.ST.Safe
 import Data.Bits
+import Control.Monad
 import Control.Monad.ST
 import qualified Data.Array.Unboxed as U
 
-qsort :: [Int] -> [Int]
-qsort lst = U.elems a
-  where
-    ll = pred $ length lst
-    a = runSTUArray $ do
-      x <- newListArray (0, ll) lst :: ST s (STUArray s Int Int)
+qsort :: (Ord e, MArray a e m, Ix i, Integral i) => a i e -> [i] -> m ()
+qsort x rnds = do
+  bds@(l',r') <- getBounds x
+  let
+    srt l r (y:ys) = do
+      xm <- readArray x (l + (mod y (r - l + 1)))
       let
-        srt l r = do
-          xm <- readArray x ((2 * l + r) `div` 3)
-          let
-            f i j
-              | i > j = return (i, j)
-              | otherwise = do
-                   let g' i = do
-                         xi <- readArray x i
-                         if xm > xi then g' (succ i) else return i
-                       g'' j = do
-                         xj <- readArray x j
-                         if xm < xj then g'' (pred j) else return j
-                   i' <- g' i
-                   j' <- g'' j
-                   if i' <= j' then do
-                     v1 <- readArray x i'
-                     v2 <- readArray x j'
-                     writeArray x i' v2
-                     writeArray x j' v1
-                     f (succ i') (pred j')
-                   else f i' j'
-          (i'', j'') <- f l r
-          if l < j'' then srt l j'' else return ()
-          if i'' < r then srt i'' r else return ()
-          return ()
-      srt 0 ll
-      return x
+        f i j
+          | i > j = return (i, j)
+          | otherwise = do
+               let g' i = do
+                     xi <- readArray x i
+                     if xm > xi then g' (succ i) else return i
+                   g'' j = do
+                     xj <- readArray x j
+                     if xm < xj then g'' (pred j) else return j
+               i' <- g' i
+               j' <- g'' j
+               if i' <= j' then do
+                 v1 <- readArray x i'
+                 v2 <- readArray x j'
+                 writeArray x i' v2
+                 writeArray x j' v1
+                 f (succ i') (pred j')
+               else return (i', j')
+      (i'', j'') <- f l r
+      when (l < j'') $ srt l j'' ys
+      when (i'' < r) $ srt i'' r ys
+  srt l' r' rnds
 
 lowerbound a x = loop (pred l') (succ h')
   where
