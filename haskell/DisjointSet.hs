9cf05c80adb37b94576cf4176ad26dc1ca3af152
antma <antma@users.noreply.github.com>
2019-03-01 17:23:59 +0300

    haskell/DisjointSet.hs: add function types

--- haskell/DisjointSet.hs
+++ haskell/DisjointSet.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts, Safe #-}
 module DisjointSet (
   disjointSetInit, disjointSetFind, disjointSetMerge
 ) where
@@ -5,11 +6,13 @@ module DisjointSet (
 import Control.Monad.ST
 import Data.Array.ST.Safe
 
+disjointSetInit :: Int -> ST s (STUArray s Int Int, STUArray s Int Int)
 disjointSetInit n = do
   p <- newListArray (0, n) [0 .. n] :: ST s (STUArray s Int Int)
   h <- newArray (0, n) 0 :: ST s (STUArray s Int Int)
   return (p, h)
 
+disjointSetFind :: (MArray a Int m) => (a Int Int, t) -> Int -> m Int
 disjointSetFind d@(p, _) x = do
   px <- readArray p x
   if px == x then return x
@@ -18,6 +21,7 @@ disjointSetFind d@(p, _) x = do
     writeArray p x px
     return px
 
+disjointSetMerge :: (MArray a Int m) => (a Int Int, a Int Int) -> Int -> Int -> m (Bool)
 disjointSetMerge d@(p, h) i j = do
   i' <- disjointSetFind d i
   j' <- disjointSetFind d j
