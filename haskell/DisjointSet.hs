5d3d5eb2b1c06daf403a96c0abf53ea5c859e6f6
antma <antma@users.noreply.github.com>
2019-03-10 20:46:40 +0300

    DisjointSet.hs: signatures fixes

--- haskell/DisjointSet.hs
+++ haskell/DisjointSet.hs
@@ -5,14 +5,18 @@ module DisjointSet (
 
 import Control.Monad.ST
 import Data.Array.ST.Safe
+import Data.Int
 
-disjointSetInit :: Int -> ST s (STUArray s Int Int, STUArray s Int Int)
+type DisjointSet s = (STUArray s Int Int, STUArray s Int Int32)
+
+disjointSetInit :: Int -> ST s (DisjointSet s)
 disjointSetInit n = do
-  p <- newListArray (0, n) [0 .. n] :: ST s (STUArray s Int Int)
-  h <- newArray (0, n) 0 :: ST s (STUArray s Int Int)
+  let n' = pred n
+  p <- newListArray (0, n') [0 .. n'] :: ST s (STUArray s Int Int)
+  h <- newArray (0, n') 0 :: ST s (STUArray s Int Int32)
   return (p, h)
 
-disjointSetFind :: (MArray a Int m) => (a Int Int, t) -> Int -> m Int
+disjointSetFind :: DisjointSet s -> Int -> ST s Int
 disjointSetFind d@(p, _) x = do
   px <- readArray p x
   if px == x then return x
@@ -21,7 +25,7 @@ disjointSetFind d@(p, _) x = do
     writeArray p x px
     return px
 
-disjointSetMerge :: (MArray a Int m) => (a Int Int, a Int Int) -> Int -> Int -> m (Bool)
+disjointSetMerge :: DisjointSet s -> Int -> Int -> ST s Bool
 disjointSetMerge d@(p, h) i j = do
   i' <- disjointSetFind d i
   j' <- disjointSetFind d j
