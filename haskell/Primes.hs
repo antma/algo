05f4c00c85c4ba8e91374b236776995c229171e2
antma <antma@users.noreply.github.com>
2019-03-13 00:06:25 +0300

    Primes.hs: remove brackets

--- haskell/Primes.hs
+++ haskell/Primes.hs
@@ -23,17 +23,17 @@ primeArray n = runSTUArray $ do
   writeArray a 0 True
   forM_ [1 .. pred hm] $ \p -> do
     l <- readArray a p
-    when (not l) $ do
+    unless l $ do
       let k = succ $ 2 * p
           loop j = when (j < m) $ do
                      writeArray a j True
                      loop (j + k)
-      loop (2 * p * (succ p))
+      loop (2 * p * succ p)
   return a
 
-isprime p x = if even x then x == 2 else not (p ! (shiftR x 1))
+isprime p x = if even x then x == 2 else not (p ! shiftR x 1)
 
-primes n = 2 : (filter ( not . (pa ! ) . (`shiftR` 1))) [3, 5 .. n]
+primes n = 2 : filter ( not . (pa ! ) . (`shiftR` 1)) [3, 5 .. n]
   where
     pa = primeArray n
 
@@ -42,7 +42,7 @@ factors p n = loop n p []
     loop 1 _ r = r
     loop m l@(x:xs) r
       | x * x > m = m : r
-      | (mod m x) == 0 = loop (div m x) l (x:r)
+      | mod m x == 0 = loop (div m x) l (x:r)
       | otherwise = loop m xs r
 
 factorization p n = map (\l -> (head l, length l)) $ group $ factors p n
