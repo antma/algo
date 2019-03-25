103d8d99ee5f0ff3f9ba72aab548dd35a74a1f92
antma <antma@users.noreply.github.com>
2019-03-26 02:40:10 +0300

    DisjointSet.hs: bang patterns

--- haskell/DisjointSet.hs
+++ haskell/DisjointSet.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, Safe #-}
+{-# LANGUAGE BangPatterns, FlexibleContexts, Safe #-}
 module DisjointSet (
   disjointSetInit, disjointSetFind, disjointSetMerge
 ) where
@@ -10,14 +10,14 @@ import Data.Int
 type DisjointSet s = (STUArray s Int Int, STUArray s Int Int32)
 
 disjointSetInit :: Int -> ST s (DisjointSet s)
-disjointSetInit n = do
-  let n' = pred n
+disjointSetInit !n = do
+  let !n' = pred n
   p <- newListArray (0, n') [0 .. n'] :: ST s (STUArray s Int Int)
   h <- newArray (0, n') 0 :: ST s (STUArray s Int Int32)
   return (p, h)
 
 disjointSetFind :: DisjointSet s -> Int -> ST s Int
-disjointSetFind d@(p, _) x = do
+disjointSetFind d@(p, _) !x = do
   px <- readArray p x
   if px == x then return x
   else do
@@ -26,7 +26,7 @@ disjointSetFind d@(p, _) x = do
     return px
 
 disjointSetMerge :: DisjointSet s -> Int -> Int -> ST s Bool
-disjointSetMerge d@(p, h) i j = do
+disjointSetMerge d@(p, h) !i !j = do
   i' <- disjointSetFind d i
   j' <- disjointSetFind d j
   if i' /= j' then do
@@ -36,6 +36,6 @@ disjointSetMerge d@(p, h) i j = do
     else if hi > hj then writeArray p j' i'
     else do
       writeArray p i' j'
-      writeArray h j' (succ hj)
+      writeArray h j' $! succ hj
     return True
   else return False
