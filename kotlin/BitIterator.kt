12bf66d6607118f1581c2014de85d1c5176b02d4
antma <antma@users.noreply.github.com>
2019-06-07 17:13:10 +0300

    BitIterator.kt: BitIteratorInt

--- kotlin/BitIterator.kt
+++ kotlin/BitIterator.kt
@@ -1,3 +1,25 @@
+object DeBruijnInt {
+  const val db = 0x4653ADF
+  private val t = IntArray(32)
+  init {
+    for (k in 0 until 32) {
+      t[(db shl k) ushr 27] = k
+    }
+  }
+  fun lookup(x: Int) = t[(x * db) ushr 27]
+}
+
+class BitIteratorInt(l: Int) : Iterator<Int> {
+  private var i = l
+  override operator fun hasNext() = i != 0
+  override operator fun next(): Int {
+    val u = i and (i - 1)
+    val r = DeBruijnInt.lookup(i xor u)
+    i = u
+    return r
+  }
+}
+
 object DeBruijnLong {
   const val db = 0x218A392CD3D5DBFL
   private val t = IntArray(64)
