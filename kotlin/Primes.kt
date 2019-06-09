258df90b7f43f4ad563ac562a99639aad7141e17
antma <antma@users.noreply.github.com>
2019-06-09 21:44:11 +0300

    Primes.kt

---
+++ kotlin/Primes.kt
@@ -0,0 +1,32 @@
+import java.util.BitSet
+import kotlin.math.*
+
+fun primes(n: Int) = PrimeTable(n).primes()
+
+class PrimeTable(val n: Int) {
+  private val m = max(1, n / 2)
+  val a = BitSet(m)
+  init {
+    a[0] = true
+    for (i in 1..ceil(sqrt(n.toDouble())).toInt()) {
+      if (!a[i]) {
+        val k = 2 * i + 1
+        for (j in 2 * i * (i + 1) until m step k) {
+          a[j] = true
+        }
+      }
+    }
+    a.flip(0, m)
+  }
+  fun isPrime (i: Int): Boolean = if ((i and 1) != 0) a[i ushr 1] else i == 2
+  fun primes(): IntArray {
+    val l = IntArray(a.cardinality()+1)
+    var k = 0
+    l[0] = 2
+    for (i in a.stream()) {
+      k++
+      l[k] = 2 * i + 1
+    }
+    return l
+  }
+}
