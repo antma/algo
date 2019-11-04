53f2575e36c010286007721ebf77e856c4f9f098
antma <antma@users.noreply.github.com>
2019-11-05 00:37:51 +0300

    NumberTheory.kt: powMod

--- kotlin/src/main/kotlin/NumberTheory.kt
+++ kotlin/src/main/kotlin/NumberTheory.kt
@@ -1,5 +1,20 @@
 import kotlin.math.*
 
+//p^q (mod m)
+fun powMod (p: Int, q: Int, m: Int): Int {
+  var a = 1L
+  var b = p.toLong()
+  var y = q
+  while (y > 0) {
+    if ((y and 1) != 0) {
+      a = (a * b) % m
+    }
+    b = (b * b) % m
+    y = y ushr 1
+  }
+  return a.toInt();
+}
+
 fun sieveArray(n: Int): IntArray {
   val a = IntArray (n + 1) { if ((it and 1) == 1 || it == 0) it else 2 }
   for (p in 3..(sqrt(n.toDouble()) + 1e-9).toInt() step 2) {
