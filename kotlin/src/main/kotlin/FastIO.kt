d30649d4169939b6eb510fe4f6b89bb19b15d745
antma <antma@users.noreply.github.com>
2019-11-21 00:08:02 +0300

    FastIO.kt: nextLine()

--- kotlin/src/main/kotlin/FastIO.kt
+++ kotlin/src/main/kotlin/FastIO.kt
@@ -1,93 +1,75 @@
+package cpalgo.fastio
 import java.io.*
 
-fun parseUnsignedInt(s:String, i: Int, j: Int): Int {
-  var x = 0
-  for (k in i until j) {
-    val u = s[k].toInt () - 48
-    assert (u >= 0 && u < 10)
-    x = 10 * x + u
+class FastScanner(private val input: InputStream) {
+  companion object {
+    val zero = 0.toByte()
+    val cr = '\r'.toByte()
+    val nl = '\n'.toByte()
+    val BUFFER_SIZE = 0x10000
   }
-  return x
-}
-
-fun parseInt(s:String, i: Int, j: Int): Int {
-  if (s[i] == '-') {
-    var x = 0
-    for (k in (i + 1) until j) {
-      val u = s[k].toInt () - 48
-      assert (u >= 0 && u < 10)
-      x = 10 * x - u
+  private val b = ByteArray(BUFFER_SIZE)
+  private var n = 0
+  private var pos = 0
+  private var eof = false
+  fun read ():Boolean {
+    if (eof) {
+      return false
     }
-    return x
-  } else {
-    var x = 0
-    for (k in i until j) {
-      val u = s[k].toInt () - 48
-      assert (u >= 0 && u < 10)
-      x = 10 * x + u
+    pos = 0
+    n = input.read(b)
+    if (n < 0) {
+      eof = true
     }
-    return x
+    return n > 0
   }
-}
-
-fun parseUnsignedLong(s:String, i: Int, j: Int): Long {
-  var x = 0L
-  for (k in i until j) {
-    val u = s[k].toInt () - 48
-    assert (u >= 0 && u < 10)
-    x = 10 * x + u
-  }
-  return x
-}
-
-fun parseLong(s:String, i: Int, j: Int): Long {
-  if (s[i] == '-') {
-    var x = 0L
-    for (k in (i + 1) until j) {
-      val u = s[k].toInt () - 48
-      assert (u >= 0 && u < 10)
-      x = 10 * x - u
-    }
-    return x
-  } else {
-    var x = 0L
-    for (k in i until j) {
-      val u = s[k].toInt () - 48
-      assert (u >= 0 && u < 10)
-      x = 10 * x + u
-    }
-    return x
-  }
-}
-
-class FastScanner() {
-  val linesIt = BufferedReader(InputStreamReader(System.`in`), 0x100000)
-    .lineSequence()
-    .iterator()
-  val re = Regex("\\s+")
-  var pos = 0
-  var s = ""
-  inline fun <T> next(parse: (String, Int, Int) -> T):T {
-    while (true) {
-      while (pos >= s.length) {
-        s = linesIt.next ()
-        pos = 0
+  fun nextLine(k: Int = -1): String? {
+    val sb = if(k >= 0) StringBuilder(k) else StringBuilder()
+    while(true) {
+      val b = nextByte()
+      if (b == zero) {
+        return if (sb.length == 0) null else sb.toString()
       }
-      val m = re.find (s, pos)
-      if (m != null) {
-        val r = m.range
-        val e = r.endInclusive + 1
-        if (pos == r.start) {
-          pos = e
-        } else {
-          val res = parse (s, pos, r.start)
-          pos = e
-          return res
+      if (b == nl) {
+        return sb.toString()
+      }
+      if (b == cr) {
+        check (nextByte() == nl)
+        return sb.toString()
+      }
+      sb.append(b.toChar())
+    }
+  }
+  fun nextByte(): Byte {
+    if (pos >= n && !read ()) {
+      return 0
+    }
+    val r = b[pos]
+    ++pos
+    return r
+  }
+  fun nextInt():Int {
+    var b:Byte
+    do {
+      b = nextByte ()
+    } while (b <= 32)
+    if (b < 48) {
+      var t = 0
+      while (true) {
+        b = nextByte ()
+        if (b <= 32) {
+          return t
         }
-      } else {
-        val res = parse (s, pos, s.length)
-        pos = Int.MAX_VALUE
-        return res
+        t = 10 * t - (b - 48)
+      }
+    } else {
+      var t = b - 48
+      while (true) {
+        b = nextByte ()
+        if (b <= 32) {
+          return t
+        }
+        t = 10 * t + (b - 48)
       }
     }
   }
---
+++ kotlin/src/test/kotlin/FastIO.kt
@@ -0,0 +1,27 @@
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import java.io.ByteArrayInputStream
+import org.junit.Test
+
+import cpalgo.fastio.*
+
+class FastIO {
+  @Test
+  fun testReadLine() {
+    val b = byteArrayOf(0x61, 0x62, 0x61, 0x63, 0x61, 0x62, 0x61, 0x0a, 
+      0x63, 0x61, 0x74, 0x0d, 0x0a,
+      0x64, 0x6f, 0x67, 0x0a)
+    val f = FastScanner(ByteArrayInputStream(b))
+    assertEquals(f.nextLine(7)!!, "abacaba")
+    assertEquals(f.nextLine()!!, "cat")
+    assertEquals(f.nextLine()!!, "dog")
+    assertTrue(f.nextLine() == null)
+  }
+  @Test
+  fun testNextInt() {
+    val b = byteArrayOf(0x31, 0x32, 0x33, 0x34, 0x35, 0x20, 0x2d, 0x32, 0x0a)
+    val f = FastScanner(ByteArrayInputStream(b))
+    assertEquals(f.nextInt(), 12345)
+    assertEquals(f.nextInt(), -2)
+  }
+}
