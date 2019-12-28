b744b4c3be7b7dddfe1b37fa579357a7c7bb51cb
antma <antma@users.noreply.github.com>
2019-12-28 19:44:50 +0300

    PolyHash.d: fix Thue-Morse antitest

--- dlang/PolyHash.d
+++ dlang/PolyHash.d
@@ -1,33 +1,55 @@
 import std.array;
 import std.conv;
 import std.random;
+import core.checkedint;
+
+struct HashT {
+  enum p1 = 4294967291U;
+  enum p2 = 4294967279U;
+  uint r1, r2;
+  static uint addm (const uint x, const uint y, const uint m) {
+    bool o;
+    immutable uint r = addu (x, y, o);
+    return (o || r >= m) ? (r - m) : r;
+  }
+  static uint subm (const uint x, const uint y, const uint m) {
+    bool o;
+    immutable uint r = subu (x, y, o);
+    return o ? (r + m) : r;
+  }
+  HashT opBinary (string op : "+")(in HashT rhs) const {
+    return HashT (addm (r1, rhs.r1, p1), addm (r2, rhs.r2, p2));
+  }
+  HashT opBinary (string op : "-")(in HashT rhs) const {
+    return HashT (subm (r1, rhs.r1, p1), subm (r2, rhs.r2, p2));
+  }
+  HashT opBinary (string op : "*")(in HashT rhs) const {
+    return HashT (((r1.to!ulong * rhs.r1) % p1).to!uint, ((r2.to!ulong * rhs.r2) % p2).to!uint);
+  }
+  ulong get () const {
+    return (r1.to!ulong << 32) + r2;
+  }
+}
 
 final class PolyHash {
   private:
-  static immutable int[] primes = [
-    2147483647, 2147483629, 2147483587, 2147483579, 2147483563, 2147483549,
-    2147483543, 2147483497, 2147483489, 2147483477, 2147483423, 2147483399,
-    2147483353, 2147483323, 2147483269, 2147483249, 2147483237, 2147483179,
-    2147483171, 2147483137];
-  immutable int n;
-  ulong[] h, d;
+  HashT[] h, d;
   public:
-  static int randomP () {
-    return primes[uniform (0, primes.length)];
-  }
-  this (in string s, int p) {
-    n = s.length.to!int;
-    h = uninitializedArray!(ulong[]) (n + 1);
-    d = uninitializedArray!(ulong[]) (n + 1);
-    h[0] = 0;
-    d[0] = 1;
+  this (in string s) {
+    immutable n = s.length.to!int;
+    auto p = HashT (uniform (256, int.max), uniform (256, int.max));
+    h = uninitializedArray!(HashT[]) (n + 1);
+    d = uninitializedArray!(HashT[]) (n + 1);
+    h[0] = HashT (0, 0);
+    d[0] = HashT (1, 1);
     foreach (i; 0 .. n) {
-      h[i+1] = h[i] * p + s[i].to!int;
+      uint c = s[i].to!uint;
+      h[i+1] = h[i] * p + HashT (c, c);
       d[i+1] = d[i] * p;
     }
   }
   //[l, r)
   ulong get (int l, int r) const {
-    return h[r] - h[l] * d[r - l];
+    return (h[r] - h[l] * d[r - l]).get ();
   }
 }
