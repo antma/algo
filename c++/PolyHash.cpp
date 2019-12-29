2e54ff88688296b0a46d80d1a9c45079a82f8e71
antma <antma@users.noreply.github.com>
2019-12-29 19:51:31 +0300

    PolyHash.cpp: fix for the case of the Thue-Morse sequence

--- c++/PolyHash.cpp
+++ c++/PolyHash.cpp
@@ -4,34 +4,60 @@
 #include <random>
 #include <cstdint>
 #include <ctime>
+#include <random>
 
 using namespace std;
 
+struct HashT {
+  static constexpr uint32_t p1 = 4294967291U;
+  static constexpr uint32_t p2 = 4294967279U;
+  uint32_t r1, r2;
+  static inline uint32_t addm (const uint32_t x, const uint32_t y, const uint32_t m) {
+    uint32_t r = x + y;
+    return (r < x || r >= m) ? (r - m) : r;
+  }
+  static inline uint32_t subm (const uint32_t x, const uint32_t y, const uint32_t m) {
+    uint32_t r = x - y;
+    return (x < y) ? (r + m) : r;
+  }
+  static inline uint32_t mulm (const uint32_t x, const uint32_t y, const uint32_t m) {
+    return (static_cast<uint64_t>(x) * y) % m;
+  }
+  auto operator+ (const HashT rhs) const {
+    return HashT (addm (r1, rhs.r1, p1), addm (r2, rhs.r2, p2));
+  }
+  auto operator- (const HashT rhs) const {
+    return HashT (subm (r1, rhs.r1, p1), subm (r2, rhs.r2, p2));
+  }
+  auto operator* (const HashT rhs) const {
+    return HashT (mulm (r1, rhs.r1, p1), mulm (r2, rhs.r2, p2));
+  }
+  uint64_t get () const {
+    return (static_cast<uint64_t> (r1) << 32) + r2;
+  }
+  HashT () : r1 (0), r2 (0) {}
+  HashT (const uint32_t _r1, const uint32_t _r2) : r1 (_r1), r2 (_r2) {}
+};
+
 class PolyHash {
   private:
-  int n;
-  vector<uint64_t> h;
-  vector<uint64_t> d;
+  const string::size_type n;
+  vector<HashT> h, d;
   public:
-  static uint32_t randomP () {
-    static int primes[20] = { 2147483647, 2147483629, 2147483587, 2147483579, 2147483563, 2147483549, 2147483543, 2147483497, 2147483489, 2147483477, 2147483423, 2147483399, 2147483353, 2147483323, 2147483269, 2147483249, 2147483237, 2147483179, 2147483171, 2147483137};
-    mt19937 gen (time (0));
-    return primes[uniform_int_distribution<int> (0, 19) (gen)];
-  }
-  PolyHash (const string &s, uint32_t p) :
-    n (s.length ()),
-    h (n + 1),
-    d (n + 1)
-  {
-    h[0] = 0;
-    d[0] = 1;
-    for (int i = 0; i < n; ++i) {
-      h[i+1] = h[i] * p + s[i];
+  PolyHash (const string s, mt19937 &rnd) : n (s.size ()), h (n + 1), d (n + 1) {
+    auto n = s.size ();
+    uniform_int_distribution<int> uid (256, 0x7fffffff);
+    const HashT p (uid (rnd), uid (rnd));
+    h[0] = HashT (0, 0);
+    d[0] = HashT (1, 1);
+    for (unsigned i = 0; i < n; ++i) {
+      uint32_t c = s[i];
+      h[i+1] = h[i] * p + HashT (c, c);
       d[i+1] = d[i] * p;
     }
   }
   //[l, r)
-  uint64_t get (int l, int r) {
-    return h[r] - h[l] * d[r - l];
+  uint64_t operator() (const int l, const int r) const {
+    return (h[r] - h[l] * d[r - l]).get ();
   }
 };
