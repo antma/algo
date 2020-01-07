638f629f6a2cd971de324657ed0f9cf8ae72f07c
antma <antma@users.noreply.github.com>
2020-01-07 23:57:11 +0300

    IntM.cpp: operator+=

--- c++/IntM.cpp
+++ c++/IntM.cpp
@@ -18,7 +18,33 @@ int gcdExt (int a, int b, int &x, int &y) {
 struct IntM {
   static constexpr uint32_t m = 1000000007U;
   uint32_t v;
-  IntM (const uint32_t x = 0) : v (x) {}
+  IntM () {}
+  IntM (const uint32_t x) : v (x) {}
+  IntM& operator+= (const IntM x) & {
+    v += x.v;
+    if (v >= m) v -= m;
+    return *this;
+  }
+  IntM& operator-= (const IntM x) & {
+    if (v < x.v) v += m;
+    v -= x.v;
+    return *this;
+  }
+  IntM& operator*= (const IntM x) & {
+    v = static_cast<uint32_t> ((static_cast<uint64_t> (v) * x.v) % m);
+    return *this;
+  }
+  auto operator+ (const IntM x) const {
+    uint32_t r = v + x.v;
+    if (r >= m) r -= m;
+    return IntM (r);
+  }
+  auto operator- (const IntM x) const {
+    uint32_t r = v - x.v;
+    if (v < x.v) r += m;
+    return IntM (r);
+  }
+  auto operator* (const IntM x) const { return IntM (static_cast<uint32_t> ((static_cast<uint64_t> (v) * x.v) % m)); }
   IntM inversion () const {
     int x, y, g = gcdExt (m, v, x, y);
     assert (g == 1);
@@ -27,16 +53,3 @@ struct IntM {
   }
 };
 ostream& operator<<(ostream &o, const IntM x) { return o << x.v; }
-IntM operator+ (const IntM x, const IntM y) {
-  uint32_t r = x.v + y.v;
-  if (r >= IntM::m) r -= IntM::m;
-  return IntM (r);
-}
-IntM operator- (const IntM x, const IntM y) {
-  uint32_t r = x.v - y.v;
-  if (x.v < y.v) r += IntM::m;
-  return IntM (r);
-}
-IntM operator* (const IntM x, const IntM y) {
-  return IntM (static_cast<uint32_t> ((static_cast<uint64_t> (x.v) * y.v) % IntM::m));
-}
