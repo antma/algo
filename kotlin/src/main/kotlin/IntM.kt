b93405cbdf77efe06753d87835f244cf90b08bb7
antma <antma@users.noreply.github.com>
2020-02-25 20:40:27 +0300

    IntM.kt: inversion()

--- kotlin/src/main/kotlin/IntM.kt
+++ kotlin/src/main/kotlin/IntM.kt
@@ -5,3 +5,16 @@ fun addm(x: Int, y: Int): Int {
 }
 
 fun mulm(x: Int, y: Int): Int = ((x.toLong() * y) % modulo).toInt()
+
+fun gcdext(a: Int, b: Int): Pair<Int, Int> {
+  if(b == 0) return Pair(1, 0)
+  val t = a / b
+  val (y, x) = gcdext(b, a - t * b)
+  return Pair(x, y - x * t)
+}
+
+fun inversion(v: Int): Int {
+  val (_, x) = gcdext (modulo, v)
+  val r = if (x < 0) x + modulo else x
+  return r
+}
