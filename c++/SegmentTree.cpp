8aa9c596ddcb119014630a2a15e551aec9ececa6
antma <antma@users.noreply.github.com>
2020-04-13 01:22:22 +0300

    SegmentTree.cpp: not commutative segment tree

--- c++/SegmentTree.cpp
+++ c++/SegmentTree.cpp
@@ -39,6 +39,44 @@ template <typename T, class O> class SegmentTree {
   }
 };
 
+template <typename T, class O> class NonCommutativeSegmentTree {
+  private:
+  size_t n;
+  vector<T> t;
+  const T zero;
+  const O &op;
+  void build () {
+    for (auto i = n - 1; i >= 1; --i) {
+      const auto k = i << 1;
+      t[i] = op (t[k], t[k+1]);
+    }
+  }
+  public:
+  void update (size_t p, const T &v) {
+    for (t[p += n] = v; p > 1; ) {
+      p >>= 1;
+      const int k = p << 1;
+      t[p] = op (t[k], t[k+1]);
+    }
+  }
+  T reduce (size_t l, size_t r) const {
+    T res1 (zero), res2 (zero);
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) {
+        res1 = op (res1, t[l++]);
+      }
+      if (r & 1) {
+        res2 = op (t[--r], res2);
+      }
+    }
+    return op (res1, res2);
+  }
+  NonCommutativeSegmentTree (const vector<T> &a, const T &_zero, const O &o) : n (a.size ()), t (2 * n), zero (_zero), op (o) {
+    copy (a.cbegin (), a.cend (), t.begin () + n);
+    build ();
+  }
+};
+
 template <typename T> class SegmentTreeSliceIncrement {
   vector<T> t;
   size_t n;
