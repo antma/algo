7160e0587e6e1200907c5c6b10890a3ed06ad2a9
antma <antma@users.noreply.github.com>
2020-05-08 16:56:56 +0300

    IntM.kt: powm

--- kotlin/src/main/kotlin/IntM.kt
+++ kotlin/src/main/kotlin/IntM.kt
@@ -13,6 +13,26 @@ fun gcdext(a: Int, b: Int): Pair<Int, Int> {
   return Pair(x, y - x * t)
 }
 
+fun powm(x: Int, p: Int): Int {
+  if(p == 0) return 1
+  var b = x
+  var y = p
+  while(0 == y and 1) {
+    b = mulm(b, b)
+    y = y ushr 1
+  }
+  var a = b
+  y = y ushr 1
+  while(y != 0) {
+    b = mulm(b, b)
+    if(0 != y and 1) {
+      a = mulm(a, b)
+    }
+    y = y ushr 1
+  }
+  return a
+}
+
 fun inversion(v: Int): Int {
   val (_, x) = gcdext (modulo, v)
   val r = if (x < 0) x + modulo else x
