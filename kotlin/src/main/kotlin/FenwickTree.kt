b2bce6f4a21ddbc0005342297712f641754661d4
antma <antma@users.noreply.github.com>
2020-05-19 12:11:17 +0300

    FenwickTree.kt: use plus as operation

--- kotlin/src/main/kotlin/FenwickTree.kt
+++ kotlin/src/main/kotlin/FenwickTree.kt
@@ -1,19 +1,19 @@
-class IntFenwickTree(val n: Int, private val zero: Int, private val op: (Int, Int) -> Int) {
+class IntFenwickTree(val n: Int) {
   private val a = IntArray(n)
   fun update(x: Int, v: Int) {
     assert(x < n)
     var i = x
     while (i < n) {
-      a[i] = op (a[i], v)
+      a[i] += v
       i = i or (i + 1)
     }
   }
   fun reduce(x: Int): Int {
     assert(x < n)
-    var r = zero
+    var r = 0
     var i = x
     while (i >= 0) {
-      r = op (r, a[i])
+      r += a[i]
       i = (i and (i + 1)) - 1
     }
     return r
