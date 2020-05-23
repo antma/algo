71dbdcfecea073759a0bb153f16e482bf55131af
antma <antma@users.noreply.github.com>
2020-05-23 21:21:12 +0300

    FastIo.scala

---
+++ scala/FastIo.scala
@@ -0,0 +1,48 @@
+object FastScanner {
+  val zero = 0.toByte
+  val cr = '\r'.toByte
+  val nl = '\n'.toByte
+  val BUFFER_SIZE = 0x10000
+}
+
+class FastScanner(private val input: java.io.InputStream) {
+  private val b = new Array[Byte](FastScanner.BUFFER_SIZE)
+  private var n = 0
+  private var pos = 0
+  private var eof = false
+  private def read ():Boolean = {
+    if (eof) {
+      return false
+    }
+    pos = 0
+    n = input.read(b)
+    if (n < 0) {
+      eof = true
+    }
+    n > 0
+  }
+  private def nextByte(): Byte = {
+    if (pos >= n && !read ()) {
+      0
+    } else {
+      val r = b(pos)
+      pos += 1
+      r
+    }
+  }
+  @tailrec
+  private def skipBlanks(): Byte = {
+    val b = nextByte()
+    if(b > 32 || b < 1) b else skipBlanks()
+  }
+  def nextInt():Int = {
+    val b = skipBlanks()
+    @tailrec
+    def f(t: Int): Int = {
+      val b = nextByte ()
+      if (b <= 32) t else f(10 * t + (b - 48))
+    }
+    require(b > 0)
+    if (b < 48) -f(0) else f(b - 48)
+  }
+}
