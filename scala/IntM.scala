180e301b15da2092ee4985f4ff4848f4b0ae70b6
antma <antma@users.noreply.github.com>
2020-06-15 01:53:06 +0300

    IntM.scala: AnyVal

--- scala/IntM.scala
+++ scala/IntM.scala
@@ -16,19 +16,18 @@ object IntM {
   val one = new IntM (1)
 }
 
-class IntM (val v: Int) {
-  def inverse = Gcd.gcdext (IntM.q, v)._3
-  def + (rhs: IntM) = new IntM ((v + rhs.v) % IntM.q)
-  def - (rhs: IntM) = new IntM ((v - rhs.v + IntM.q) % IntM.q)
+class IntM private (val v: Int) extends AnyVal {
+  def inverse = IntM.fromInt(Gcd.gcdext (IntM.q, v)._3)
+  def + (rhs: IntM) = {
+    val z = v + rhs.v
+    new IntM(if(z >= IntM.q) z - IntM.q else z)
+  }
+  def - (rhs: IntM) = new IntM (if (v >= rhs.v) v - rhs.v else v - rhs.v + IntM.q)
   def * (rhs: IntM) = new IntM (((v.toLong * rhs.v.toLong) % IntM.q).toInt)
   def / (rhs: IntM): IntM = {
     assert (rhs.v > 0)
     return this * rhs.inverse
   }
-  def + (rhs: Int): IntM = this + (IntM.fromInt (rhs))
-  def - (rhs: Int): IntM = this - (IntM.fromInt (rhs))
-  def * (rhs: Int): IntM = this * (IntM.fromInt (rhs))
-  def / (rhs: Int): IntM = this / (IntM.fromInt (rhs))
   def pow (exp: Int): IntM = {
     var a = IntM.one
     var b = this
@@ -42,6 +41,5 @@ class IntM (val v: Int) {
     }
     a
   }
-  def equals (rhs: IntM) = v == rhs.v
   override def toString = v.toString
 }
