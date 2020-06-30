01623f2c40fc9dfb63386c70f94d6d29cf45057d
antma <antma@users.noreply.github.com>
2020-07-01 02:27:58 +0300

    PrimeTableTest.scala

---
+++ scala/src/test/scala/PrimeTableTest.scala
@@ -0,0 +1,16 @@
+package org.github.antma.cpalgo
+import org.specs2.mutable.Specification
+
+class PrimeTableTest extends Specification {
+  "PrimeTable" should {
+     "sum of primes <1000000" in {
+       new PrimeTable(1000000).primes.foldRight(0L){ _ + _} must_== 37550402023L
+     }
+     "primes below 200 tests" in {
+       val p = List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199)
+       (1 until 200).forall { n =>
+         new PrimeTable(n).primes.sameElements(p.takeWhile { _ < n })
+       } must beTrue
+     }
+  }
+}
