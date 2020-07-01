ea7ea0880dd00c0b067a7f5a1bf1e89c26e20187
antma <antma@users.noreply.github.com>
2020-07-01 23:40:52 +0300

    FastIO.scala: Scanner.nextLine(), buffered output

--- scala/FastIO.scala
+++ scala/FastIO.scala
@@ -3,63 +3,88 @@ package org.github.antma.cpalgo
 import scala.annotation.tailrec
 import scala.collection.mutable.StringBuilder
 
-private object FastScanner {
-  val cr = '\r'.toByte
-  val nl = '\n'.toByte
-  val BUFFER_SIZE = 0x10000
+object FastIO {
+  private val BUFFER_SIZE = 0x100000
+  private val cr = '\r'.toInt
+  private val nl = '\n'.toInt
+  class Scanner private[FastIO](input: java.io.InputStream) {
+    private val r = new java.io.BufferedInputStream(input, BUFFER_SIZE)
+    @tailrec
+    private def skipBlanks(): Int = {
+      val b = r.read()
+      if(b > 32 || b < 0) b else skipBlanks()
+    }
+    final def nextLine(k: Int = -1): Option[String] = {
+      val sb = if(k >= 0) new StringBuilder(k) else new StringBuilder()
+      @tailrec
+      def loop(): Option[String] = {
+        val b = r.read()
+        if (b < 0) {
+          if (sb.isEmpty) None else Some(sb.result())
+        } else if (b == nl) {
+          Some(sb.result())
+        } else if (b == cr) {
+          require(r.read() == nl, "expected newline after carrage return")
+          Some(sb.result())
+        } else {
+          sb.append(b.toChar)
+          loop()
+        }
+      }
+      loop()
+    }
+    final def nextToken(k: Int = -1): String = {
+      val sb = if(k >= 0) new StringBuilder(k) else new StringBuilder()
+      var b = skipBlanks()
+      require(b >= 0, "unexpected EOF")
+      while(b > 32) {
+        sb.append(b.toChar)
+        b = r.read()
+      }
+      sb.toString
+    }
+    final def nextDouble(): Double = {
+      @tailrec
+      def g(t: Double, x: Double): Double = {
+        val b = r.read ()
+        if (b <= 32) t * x else g(10 * t + (b - 48), x * 0.1)
+      }
+      @tailrec
+      def f(t: Double): Double = {
+        val b = r.read ()
+        if(b == 46) g(t, 1.0)
+        else if (b <= 32) t else f(10 * t + (b - 48))
+      }
+      val b = skipBlanks()
+      require(b >= 0, "unexpected EOF")
+      if (b < 48) -f(0) else f(b - 48)
+    }
+    final def nextInt():Int = {
+      val b = skipBlanks()
+      @tailrec
+      def f(t: Int): Int = {
+        val b = r.read ()
+        if (b <= 32) t else f(10 * t + (b - 48))
+      }
+      require(b >= 0, "unexpected EOF")
+      if (b < 48) -f(0) else f(b - 48)
+    }
+    final def nextLong():Long = {
+      val b = skipBlanks()
+      @tailrec
+      def f(t: Long): Long = {
+        val b = r.read ()
+        if (b <= 32) t else f(10 * t + (b - 48))
+      }
+      require(b >= 0, "unexpected EOF")
+      if (b < 48) -f(0) else f(b - 48)
+    }
+  }
+  final def newScanner(input: java.io.InputStream) = new Scanner(input)
+  final def newWriter() = {
+    val osw = new java.io.OutputStreamWriter(System.out, java.nio.charset.StandardCharsets.US_ASCII)
+    val bw = new java.io.BufferedWriter(osw, BUFFER_SIZE)
+    new java.io.PrintWriter(bw, false)
+  }
 }
 
-class FastScanner(input: java.io.InputStream) {
-  private val r = new java.io.BufferedInputStream(input, FastScanner.BUFFER_SIZE)
-  @tailrec
-  private def skipBlanks(): Int = {
-    val b = r.read()
-    if(b > 32 || b < 0) b else skipBlanks()
-  }
-  final def nextToken(k: Int = -1): String = {
-    val sb = if(k >= 0) new StringBuilder(k) else new StringBuilder()
-    var b = skipBlanks()
-    require(b >= 0, "unexpected EOF")
-    while(b > 32) {
-      sb.append(b.toChar)
-      b = r.read()
-    }
-    sb.toString
-  }
-  final def nextDouble(): Double = {
-    @tailrec
-    def g(t: Double, x: Double): Double = {
-      val b = r.read ()
-      if (b <= 32) t * x else g(10 * t + (b - 48), x * 0.1)
-    }
-    @tailrec
-    def f(t: Double): Double = {
-      val b = r.read ()
-      if(b == 46) g(t, 1.0)
-      else if (b <= 32) t else f(10 * t + (b - 48))
-    }
-    val b = skipBlanks()
-    require(b >= 0, "unexpected EOF")
-    if (b < 48) -f(0) else f(b - 48)
-  }
-  final def nextInt():Int = {
-    val b = skipBlanks()
-    @tailrec
-    def f(t: Int): Int = {
-      val b = r.read ()
-      if (b <= 32) t else f(10 * t + (b - 48))
-    }
-    require(b >= 0, "unexpected EOF")
-    if (b < 48) -f(0) else f(b - 48)
-  }
-  final def nextLong():Long = {
-    val b = skipBlanks()
-    @tailrec
-    def f(t: Long): Long = {
-      val b = r.read ()
-      if (b <= 32) t else f(10 * t + (b - 48))
-    }
-    require(b >= 0, "unexpected EOF")
-    if (b < 48) -f(0) else f(b - 48)
-  }
-}
--- scala/src/test/scala/FastIOTest.scala
+++ scala/src/test/scala/FastIOTest.scala
@@ -2,9 +2,9 @@ package org.github.antma.cpalgo
 import org.specs2.mutable.Specification
 
 class FastIOTest extends Specification {
-  def fromString(s: String) = new FastScanner(new java.io.ByteArrayInputStream(s.toCharArray.map { _.toByte }))
+  def fromString(s: String) = FastIO.newScanner(new java.io.ByteArrayInputStream(s.toCharArray.map { _.toByte }))
   "FastIO" should {
-     "FastScanner.readInt()" in {
+     "Scanner.readInt()" in {
         val r = fromString("1 -10\r\n 2")
         r.nextInt() must be equalTo 1
         r.nextInt() must be equalTo -10
@@ -16,14 +16,21 @@ class FastIOTest extends Specification {
         }
         f must beNone
      }
-     "FastScanner.readDouble()" in {
+     "Scanner.readDouble()" in {
         val r = fromString("123.456\n-2")
         r.nextDouble() must beCloseTo(123.456, 1e-10)
         r.nextDouble() must be equalTo -2.0
      }
-     "FastScanner.readLong()" in {
+     "Scanner.readLong()" in {
         val r = fromString("-1234567890")
         r.nextLong() must be equalTo -1234567890L
+     }
+     "Scanner.nextLine()" in {
+       val r = fromString("1\n2\r\n3")
+       r.nextLine() must beSome("1")
+       r.nextLine() must beSome("2")
+       r.nextLine() must beSome("3")
+       r.nextLine() must beNone
      }
   }
 }
