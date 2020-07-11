6c8a03777672d803f590d7301d72c85121c5f0fa
antma <antma@users.noreply.github.com>
2020-07-12 02:32:52 +0300

    DisjointSet.scala: final class

--- scala/DisjointSet.scala
+++ scala/DisjointSet.scala
@@ -1,18 +1,18 @@
 package com.github.antma.cpalgo
 
-class DisjointSet (n: Int) {
-  val p = (0 until n).toArray
+final class DisjointSet(n: Int) {
+  val p = Array.range(0, n)
   val h = Array.ofDim[Int] (n)
-  def find_set (x: Int): Int = {
+  def findSet(x: Int): Int = {
     if (p(x) == x) x
     else {
-      p(x) = find_set (p(x))
+      p(x) = findSet(p(x))
       p(x)
     }
   }
-  def merge (x: Int, y: Int): Boolean = {
-    val i = find_set (x)
-    val j = find_set (y)
+  def merge(x: Int, y: Int): Boolean = {
+    val i = findSet(x)
+    val j = findSet(y)
     if (i == j) false
     else {
       if (h(i) < h(j)) {
