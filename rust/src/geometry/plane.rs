c87a55771f3ba3df74353135445e47d6a8465f58
antma <antma@users.noreply.github.com>
2020-09-29 21:56:25 +0300

    geometry/plane.rs: lines intersection and circumcircle

--- rust/src/geometry/plane.rs
+++ rust/src/geometry/plane.rs
@@ -26,6 +26,13 @@ impl<T: Sub<Output = T>> Sub for Point<T> {
   }
 }
 
+impl Point<f64> {
+  pub fn dist(&self, other: &Self) -> f64 {
+    let d = *self - *other;
+    d.x.hypot(d.y)
+  }
+}
+
 impl<T: Mul<Output = T> + Copy> Mul<T> for Point<T> {
   type Output = Self;
   fn mul(self, other: T) -> Self::Output {
@@ -33,5 +40,71 @@ impl<T: Mul<Output = T> + Copy> Mul<T> for Point<T> {
       x: self.x * other,
       y: self.y * other,
     }
+  }
+}
+
+impl<T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T>> Point<T> {
+  fn dot_product(&self, other: &Self) -> T {
+    self.x * other.x + self.y * other.y
+  }
+}
+
+//ax + by = c
+pub struct Line<T> {
+  a: T,
+  b: T,
+  c: T,
+}
+
+impl<T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T>> Line<T> {
+  pub fn new(p: &Point<T>, q: &Point<T>) -> Self {
+    let a = p.y - q.y;
+    let b = q.x - p.x;
+    Self {
+      a,
+      b,
+      c: a * p.x + b * p.y,
+    }
+  }
+}
+
+impl Line<f64> {
+  pub fn intersect(&self, other: &Self, eps: f64) -> Option<Point<f64>> {
+    let d = self.a * other.b - other.a * self.b;
+    if d.abs() < eps {
+      None
+    } else {
+      Some(
+        Point {
+          x: self.c - other.b - other.c * self.b,
+          y: self.a * other.c - other.a * self.c,
+        } * (1.0 / d),
+      )
+    }
+  }
+  pub fn bisection(p: &Point<f64>, q: &Point<f64>) -> Self {
+    let m = (*p + *q) * 0.5;
+    let d = *q - *p;
+    Self {
+      a: d.x,
+      b: d.y,
+      c: d.dot_product(&m),
+    }
+  }
+}
+
+pub struct Circle<T> {
+  pub c: Point<T>,
+  pub r: T,
+}
+
+impl Circle<f64> {
+  pub fn circumcircle(p1: &Point<f64>, p2: &Point<f64>, p3: &Point<f64>, eps: f64) -> Option<Self> {
+    let l1 = Line::bisection(p1, p2);
+    let l2 = Line::bisection(p2, p3);
+    l1.intersect(&l2, eps).map(|p| Circle {
+      c: p,
+      r: p.dist(p1),
+    })
   }
 }
