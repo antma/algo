aa33bcae204d821c08c841a1a771ca226fa154f3
antma <antma@users.noreply.github.com>
2020-10-18 00:10:16 +0300

    DisjointSet

--- rust/src/disjoint_set.rs
+++ rust/src/disjoint_set.rs
@@ -1,3 +1,46 @@
+pub struct DisjointSet {
+  p: Vec<usize>,
+  h: Vec<i32>,
+}
+
+impl DisjointSet {
+  pub fn new(n: usize) -> Self {
+    Self {
+      p: (0..n).collect(),
+      h: vec![0i32; n],
+    }
+  }
+  pub fn find_set(&mut self, x: usize) -> usize {
+    let y = self.p[x];
+    if y == x {
+      return x;
+    }
+    self.p[x] = self.find_set(y);
+    return self.p[x];
+  }
+  pub fn merge(&mut self, x: usize, y: usize) -> bool {
+    let i = self.find_set(x);
+    let j = self.find_set(y);
+    if i != j {
+      match self.h[i].cmp(&self.h[j]) {
+        std::cmp::Ordering::Less => {
+          self.p[i] = j;
+        }
+        std::cmp::Ordering::Greater => {
+          self.p[j] = i;
+        }
+        _ => {
+          self.p[i] = j;
+          self.h[j] += 1;
+        }
+      }
+      true
+    } else {
+      false
+    }
+  }
+}
+
 pub struct DisjointSetWithSize {
   p: Vec<usize>,
   h: Vec<i32>,
