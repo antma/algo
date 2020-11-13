049f8940af63239475a70da56de1ea7b58d83145
antma <antma@users.noreply.github.com>
2020-11-13 22:29:23 +0300

    intm.rs: +,-, inversion

--- rust/src/intm.rs
+++ rust/src/intm.rs
@@ -1,5 +1,38 @@
+pub fn addm(x: u32, y: u32, m: u32) -> u32 {
+  let r = x.wrapping_add(y);
+  if r < x || r >= m {
+    r.wrapping_sub(m)
+  } else {
+    r
+  }
+}
+
+pub fn subm(x: u32, y: u32, m: u32) -> u32 {
+  let r = x.wrapping_sub(y);
+  if x < y {
+    r.wrapping_add(m)
+  } else {
+    r
+  }
+}
+
 pub fn mulm(x: u32, y: u32, m: u32) -> u32 {
   (((x as u64) * (y as u64)) % (m as u64)) as u32
+}
+
+fn gcdext(a: i32, b: i32) -> (i32, i32, i32) {
+  if b == 0 {
+    (a, 1, 0)
+  } else {
+    let (res, y, x) = gcdext(b, a % b);
+    (res, x, y - x * (a / b))
+  }
+}
+
+pub fn inv(n: u32, m: u32) -> u32 {
+  let (g, _, x) = gcdext(m as i32, n as i32);
+  assert_eq!(g, 1);
+  (if x < 0 { x + m as i32 } else { x }) as u32
 }
 
 pub fn powm(x: u32, y: u32, m: u32) -> u32 {
