e2204779019d268ee185c7cc04d50adfa51573fc
antma <antma@users.noreply.github.com>
2021-04-11 01:16:49 +0300

    Geometry.hs: segments intersection

--- haskell/Geometry.hs
+++ haskell/Geometry.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE BangPatterns, Safe #-}
 module Geometry (
-  Point,
+  Point (..), Segment (..), RichSegment (..),
   Line (..), Circle (..),
   dist,
   line,
   pointInCircle,
   linesIntersection,
   circumcircle,
-  minimalEnclosingBall
+  minimalEnclosingBall,
+  (.+.), (.-.), (.*.), richSegment, richSegmentsIntersects, richSegmentContainsPoint
 ) where
 
 import Random
@@ -17,66 +18,104 @@ import Data.Int
 import Data.List
 import Data.Maybe
 
---(Line a b c): a * x + b * y = c
-eps :: Double
-eps = 1e-10
+data Point a = Point a a
+--(Line a b c): a * x + b * y + c = 0
+data Line a = Line a a a
+data Segment a = Segment (Point a) (Point a)
+data RichSegment a = RichSegment (Point a) (Point a) (Point a) (Point a) (Line a)
+data Circle a = Circle (Point a) a
 
-type Point = (Double, Double)
-data Line = Line !Double !Double !Double
-data Circle = Circle !Double !Double !Double
+pop :: (a -> a -> a) -> Point a -> Point a -> Point a
+pop f (Point x' y') (Point x'' y'') = Point (f x' x'') (f y' y'')
 
-line :: Point -> Point -> Line
-line (!x1, !y1) (!x2, !y2) = Line a b (a * x1 + b * y1)
+(.+.) :: Num a => Point a -> Point a -> Point a
+(.+.) p' p'' = pop (+) p' p''
+
+(.-.) :: Num a => Point a -> Point a -> Point a
+(.-.) p' p'' = pop (-) p' p''
+
+(.*.) :: Num a => Point a -> a -> Point a
+(.*.) (Point x y) c = Point (x * c) (y * c)
+
+minp :: Ord a => Point a -> Point a -> Point a
+minp p' p'' = pop min p' p''
+
+maxp :: Ord a => Point a -> Point a -> Point a
+maxp p' p'' = pop max p' p''
+
+infixl 6 .+., .-.
+
+line :: Num a => Point a -> Point a -> Line a
+line (Point x' y') (Point x'' y'') = Line a b c
   where
-    !a = y2 - y1
-    !b = x1 - x2
+    a = y'' - y'
+    b = x' - x''
+    c = negate $ a * x' + b * y'
 
-dist :: Point -> Point -> Double
-dist (!x1, !y1) (!x2, !y2) = sqrt $! (x1 - x2) ^ 2 +  (y1 - y2) ^ 2
+linePointSignum :: Num a => Line a -> Point a -> a
+linePointSignum (Line a b c) (Point x y) = signum $ a * x + b * y + c
 
-pointInCircle :: Point -> Circle -> Bool
-pointInCircle p (Circle !x !y !r) = dist p (x, y) - r < eps
+richSegment :: Num a => Ord a => Segment a -> RichSegment a
+richSegment (Segment p' p'') = RichSegment p' p'' (minp p' p'') (maxp p' p'') (line p' p'')
 
-linesIntersection :: Line -> Line -> Maybe (Double, Double)
-linesIntersection (Line a1 b1 c1) (Line a2 b2 c2)
+richSegmentContainsPoint :: Num a => Ord a => RichSegment a -> Point a -> Bool
+richSegmentContainsPoint (RichSegment _ _ (Point ux uy) (Point vx vy) l) p@(Point x y) =
+  linePointSignum l p == 0 && ux <= x && x <= vx && uy <= y && y <= vy
+
+richSegmentsIntersects :: Num a => Ord a => RichSegment a -> RichSegment a -> Bool
+richSegmentsIntersects (RichSegment p1' p2' u' v' l') (RichSegment p1'' p2'' u'' v'' l'') =
+  x' <= x'' && y' <= y'' && sign1' * sign2' <= 0 && sign1'' * sign2'' <= 0
+  where
+    (Point x' y')  = maxp u' u''
+    (Point x'' y'')  = minp v' v''
+    sign1' = linePointSignum l'' p1'
+    sign2' = linePointSignum l'' p2'
+    sign1'' = linePointSignum l' p1''
+    sign2'' = linePointSignum l' p2''
+
+dist :: Point Double -> Point Double -> Double
+dist p' p'' = let (Point x y) =  p' .-. p'' in sqrt $ x * x + y * y
+
+pointInCircle :: Point Double -> Circle Double -> Double -> Bool
+pointInCircle p (Circle c r) eps = dist p c - r < eps
+
+linesIntersection :: Line Double -> Line Double -> Double -> Maybe (Point Double)
+linesIntersection (Line a1 b1 c1) (Line a2 b2 c2) eps
   | abs d < eps = Nothing
-  | otherwise = let !d' = 1.0 / d in Just ( d' * (c1 * b2 - c2 * b1), d' * (a1 * c2 - a2 * c1))
+  | otherwise = Just (p .*. (1.0 / d))
   where
-    !d = a1 * b2 - a2 * b1
+    d = a1 * b2 - a2 * b1
+    p = Point (c2 * b1 - c1 * b2) (a2 * c1 - a1 * c2)
 
-circle :: Point -> Point -> Circle
-circle p1@(!x1, !y1) (!x2, !y2) = Circle x y (dist (x, y) p1)
+circle :: Point Double -> Point Double -> Circle Double
+circle p1@(Point x1 y1) (Point x2 y2) = Circle p (dist p p1)
   where
+    p = Point x y
     !x = (x1 + x2) * 0.5
     !y = (y1 + y2) * 0.5
 
-circumcircle ::  Point -> Point -> Point -> Maybe Circle
-circumcircle p1 p2 p3 = liftM (\ p@(!x, !y) -> Circle x y (dist p p1)) $ linesIntersection (cp p1 p2) (cp p2 p3)
+circumcircle :: Point Double -> Point Double -> Point Double -> Double -> Maybe (Circle Double)
+circumcircle p1 p2 p3 eps = liftM (\ p -> Circle p (dist p p1)) $ linesIntersection (cp p1 p2) (cp p2 p3) eps
   where
-    cp :: Point -> Point -> Line
-    cp (!x1, !y1) (!x2, !y2) = Line a b (a * xm + b * ym)
+    cp :: Point Double -> Point Double -> Line Double
+    cp q1 q2 = Line a b $ negate $ a * xm + b * ym
       where
-        !xm = (x1 + x2) * 0.5
-        !ym = (y1 + y2) * 0.5
-        !a = x2 - x1
-        !b = y2 - y1
+        (Point xm ym) = (q1 .+. q2) .*. 0.5
+        (Point a b) = q2 .-. q1
 
-minimalEnclosingBallP2 :: [Point] -> Point -> Point -> Circle
-minimalEnclosingBallP2 p q1 q2 = foldl' f (circle q1 q2) p
+minimalEnclosingBallP2 :: [Point Double] -> Point Double -> Point Double -> Double -> Circle Double
+minimalEnclosingBallP2 q q1 q2 eps = foldl' f (circle q1 q2) q
   where
-    f :: Circle -> Point -> Circle
-    f c p = if pointInCircle p c then c else fromJust $ circumcircle q1 q2 p
+    f c p = if pointInCircle p c eps then c else fromJust $ circumcircle q1 q2 p eps
 
-minimalEnclosingBallP1 :: [Point] -> Point -> [Int32] -> (Circle, [Int32])
-minimalEnclosingBallP1 p q1 rnds = (fst $ foldl' f ( circle q1 p', [p']) ps, rnds')
+minimalEnclosingBallP1 :: [Point Double] -> Point Double -> [Int32] -> Double -> (Circle Double, [Int32])
+minimalEnclosingBallP1 q q1 rnds eps = (fst $ foldl' f ( circle q1 p', [p']) ps, rnds')
   where
-    ( (p' : ps), rnds') = randomShuffle p rnds
-    f :: (Circle, [Point]) -> Point -> (Circle, [Point])
-    f (c, l) p = if pointInCircle p c then (c, p:l) else (minimalEnclosingBallP2 l q1 p, p:l)
+    ( (p' : ps), rnds') = randomShuffle q rnds
+    f (c, l) p = if pointInCircle p c eps then (c, p:l) else (minimalEnclosingBallP2 l q1 p eps, p:l)
 
-minimalEnclosingBall :: [Point] -> [Int32] -> Circle
-minimalEnclosingBall p rnds = let (c, _, _) = foldl' f (circle p1 p2, [p1,p2], rnds') ps in c
+minimalEnclosingBall :: [Point Double] -> [Int32] -> Double -> Circle Double
+minimalEnclosingBall q rnds eps = let (c, _, _) = foldl' f (circle p1 p2, [p1,p2], rnds') ps in c
   where
-    ( (p1 : p2 : ps), rnds') = randomShuffle p rnds
-    f :: (Circle, [Point], [Int32]) -> Point -> (Circle, [Point], [Int32])
-    f (c, l, r) p = if pointInCircle p c then (c, p:l, r) else let (c', r') = minimalEnclosingBallP1 l p r in (c', p:l, r')
+    ( (p1 : p2 : ps), rnds') = randomShuffle q rnds
+    f (c, l, r) p = if pointInCircle p c eps then (c, p:l, r) else let (c', r') = minimalEnclosingBallP1 l p r eps in (c', p:l, r')
--- haskell/Test.hs
+++ haskell/Test.hs
@@ -4,7 +4,32 @@ import Test.HUnit
 import qualified TreapSet
 import qualified Random
 import qualified Primes
+import qualified Geometry
 import Data.Int
+import Data.Maybe
+
+-------------------- Geometry --------------------
+geometry_tests :: [Test]
+geometry_tests = [_test1, _test2, _test3, _test4]
+  where
+    p1 = Geometry.Point 0.0 0.0
+    p2 = Geometry.Point 1.0 1.0
+    p3 = Geometry.Point 1.0 0.0
+    p4 = Geometry.Point 0.0 1.0
+    p6 = Geometry.Point 0.5 0.5
+    l1 = Geometry.line p1 p2
+    l2 = Geometry.line p3 p4
+    l3 = Geometry.line p1 p4
+    l4 = Geometry.line p2 p3
+    eps = 1e-10
+    (Just p5) = Geometry.linesIntersection l1 l2 eps
+    _test1 = TestCase (assertBool "two lines intersected" (abs (Geometry.dist p5 p6) < 1e-6))
+    _test2 = TestCase (assertBool "two parallel lines" (isNothing $ Geometry.linesIntersection l3 l4 eps))
+    s1 = Geometry.richSegment $ Geometry.Segment p1 p2
+    s2 = Geometry.richSegment $ Geometry.Segment p3 p4
+    Just (Geometry.Circle p7 r7) = Geometry.circumcircle p1 p3 p4 eps
+    _test3 = TestCase (assertBool "segment intersected" (Geometry.richSegmentsIntersects s1 s2))
+    _test4 = TestCase (assertBool "circumcircle" $ abs (r7 - 0.5 * sqrt 2.0) < eps && (abs (Geometry.dist p7 (Geometry.Point 0.5 0.5))< 1e-6))
 
 maket :: [Int32] -> Int32 -> TreapSet.TreapSet
 maket l seed = foldl (\acc (i, j) -> TreapSet.tsInsert i j acc) TreapSet.Nil $ zip l (Random.randoms seed)
@@ -23,10 +48,10 @@ test2 = TestCase (assertBool "check heap (maximum)" (y >= yl && y >= yr))
 test3 = TestCase (assertBool "check sumOfDivisors" (Primes.sumOfDivisors [2] 6 == 12))
 
 tests :: Test
-tests = TestList [TestLabel "test1" test1,
-                  TestLabel "test2" test2,
-                  TestLabel "test3" test3
-                 ]
+tests = TestList $ [ TestLabel "test1" test1,
+                     TestLabel "test2" test2,
+                     TestLabel "test3" test3
+                   ] ++ geometry_tests
 
 main :: IO Counts
 main = do runTestTT tests
