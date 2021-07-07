d7a9fc5b64441d4271d1f6a4d93e4ee8f7663b03
antma <antma@users.noreply.github.com>
2021-07-08 02:06:03 +0300

    move BinomialsM into number_theory/binomialsm.rs

--- rust/tests/number_theory.rs
+++ rust/tests/number_theory.rs
@@ -1,6 +1,7 @@
-use algo::number_theory::intm::BinomialsM;
+use algo::number_theory::binomialsm::BinomialsM;
+
 #[test]
-fn intm_binomials_tests() {
+fn number_theory_binomialsm_tests() {
   let b = BinomialsM::new(100, 1_000_000_007);
   assert_eq!(b.binomial(100, 50), 538992043);
   assert_eq!(b.binomial(100, 49), 273521609);
