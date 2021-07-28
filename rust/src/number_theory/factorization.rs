ba53ca6cfffc7a2840bf9539ad2ab853869bf729
antma <antma@users.noreply.github.com>
2021-07-28 15:11:23 +0300

    factorization.rs: monte-carlo, ferma

--- rust/src/number_theory/factorization.rs
+++ rust/src/number_theory/factorization.rs
@@ -1,15 +1,50 @@
+use crate::math::sqrtint;
+use crate::number_theory::exponentation::generic_pow;
 use crate::number_theory::gcd::Gcd;
 use crate::number_theory::primality_test64::is_prime64;
 use crate::random::KnuthRandom;
+use std::collections::HashMap;
 
 pub struct Factorization(pub Vec<(u64, u8)>);
+
+fn pollard_monte_carlo(
+  n: u64,
+  gcd: &mut Gcd,
+  small_primes: &[u32],
+  rnd: &mut KnuthRandom,
+) -> Option<u64> {
+  let n128 = n as u128;
+  let mut b = rnd.randrange(2..*small_primes.last().unwrap() + 1) as u64;
+  for q in small_primes {
+    let mut cur = *q as u64;
+    while let Some(w) = cur.checked_mul(*q as u64) {
+      if w > n {
+        break;
+      }
+      cur = w;
+      b = generic_pow(
+        b,
+        cur,
+        |x, y| (((x as u128) * (y as u128)) % n128) as u64,
+        || 1u64,
+      );
+      if b > 0 {
+        let g = gcd.gcd_u64(n, b - 1);
+        if g > 1 && g < n {
+          return Some(g);
+        }
+      }
+    }
+  }
+  None
+}
 
 fn pollard_rho(n: u64, gcd: &mut Gcd, rnd: &mut KnuthRandom) -> Option<u64> {
   let x0 = rnd.randrange(0..n.min(0x7fff_ffff) as u32) as u64;
   let mut y = x0;
   let mut x = x0;
   let mut k = 2;
-  let its = (n as f64).sqrt().sqrt().round() as u32 + 10;
+  let its = 3 * (n as f64).sqrt().sqrt().round() as u32 + 10;
   for i in 2..its {
     let xi = if x != 0 {
       let z = x as u128;
@@ -30,43 +65,53 @@ fn pollard_rho(n: u64, gcd: &mut Gcd, rnd: &mut KnuthRandom) -> Option<u64> {
   None
 }
 
-fn pollard_factorization(
+fn ferma(n: u64) -> u64 {
+  let mut x = sqrtint(n) as u64;
+  let mut y = 0;
+  let mut xx = x * x;
+  let mut yy = n;
+  loop {
+    if xx == yy {
+      break if x != y { x - y } else { x + y };
+    }
+    if xx > yy {
+      yy += y + y + 1;
+      y += 1;
+    } else {
+      xx += x + x + 1;
+      x += 1;
+    }
+  }
+}
+
+fn factorization_large(
   p: u64,
+  small_primes: &[u32],
   gcd: &mut Gcd,
   rnd: &mut KnuthRandom,
   tries: u32,
-) -> std::collections::HashMap<u64, u8> {
-  let mut h = std::collections::HashMap::new();
+  h: &mut HashMap<u64, u8>,
+) {
   if p == 1 {
-    return h;
+    return;
   }
   if is_prime64(p, gcd, rnd, tries) {
-    h.insert(p, 1);
-    return h;
+    *(h.entry(p).or_insert(0)) += 1;
+    return;
   }
-  loop {
-    if let Some(d) = pollard_rho(p, gcd, rnd) {
-      let d = d.min(p / d);
-      let w = pollard_factorization(d, gcd, rnd, tries);
-      let mut u = p / d;
-      for q in w.iter() {
-        while u % q.0 == 0 {
-          let e = h.entry(*q.0).or_insert(0);
-          *e += 1;
-          u = u / q.0;
-        }
-      }
-      for q in w {
-        let e = h.entry(q.0).or_insert(0);
-        *e += q.1;
-      }
-      for q in pollard_factorization(u, gcd, rnd, tries) {
-        let e = h.entry(q.0).or_insert(0);
-        *e += q.1;
-      }
-      break h;
+  let d = loop {
+    let d = pollard_monte_carlo(p, gcd, small_primes, rnd);
+    if d.is_some() {
+      break d.unwrap();
     }
-  }
+    let d = pollard_rho(p, gcd, rnd);
+    if d.is_some() {
+      break d.unwrap();
+    }
+    break ferma(p);
+  };
+  factorization_large(d, small_primes, gcd, rnd, tries, h);
+  factorization_large(p / d, small_primes, gcd, rnd, tries, h);
 }
 
 pub fn factorization64(
@@ -108,7 +153,10 @@ pub fn factorization64(
       }
     }
   }
-  r.extend(pollard_factorization(p, gcd, rnd, tries));
+  let mut h = HashMap::new();
+  let sp = &small_primes[0..small_primes.len().min(25)];
+  factorization_large(p, sp, gcd, rnd, tries, &mut h);
+  r.extend(h);
   r.sort();
   Factorization(r)
 }
