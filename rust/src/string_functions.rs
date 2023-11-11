2dbc2bda95a59258c0658b1e29ecaa7f82892034
antma <antma@users.noreply.github.com>
2023-11-11 18:56:58 +0300

    KMP

--- rust/src/string_functions.rs
+++ rust/src/string_functions.rs
@@ -18,3 +18,77 @@ pub fn compute_z_function<T: Eq>(s: &[T]) -> Vec<usize> {
   }
   z
 }
+
+pub fn compute_prefix_function<T: Eq>(s: &[T]) -> Vec<usize> {
+  let n = s.len();
+  let mut p = Vec::with_capacity(n);
+  p.push(0);
+  let mut k = 0;
+  for q in 1..n {
+    while k > 0 && s[k] != s[q] {
+      k = p[k - 1];
+    }
+    if s[k] == s[q] {
+      k += 1;
+    }
+    p.push(k);
+  }
+  p
+}
+
+fn kmp_next<T: Eq>(s: &[T], p: &Vec<usize>, state: usize, c: T) -> (bool, usize) {
+  let mut q = state;
+  while q > 0 && c != s[q] {
+    q = p[q - 1];
+  }
+  if c == s[q] {
+    q += 1;
+  }
+  if q == s.len() {
+    (true, p[q - 1])
+  } else {
+    (false, q)
+  }
+}
+
+pub struct KMPIterator<'a, T> {
+  s: &'a [T],
+  p: &'a Vec<usize>,
+  t: &'a [T],
+  state: usize,
+  pos: usize,
+}
+
+impl<'a, T: Eq + Copy> Iterator for KMPIterator<'a, T> {
+  type Item = usize;
+  fn next(&mut self) -> Option<Self::Item> {
+    let mut q = self.state;
+    let mut r: Option<Self::Item> = None;
+    for (i, c) in self.t.iter().enumerate().skip(self.pos) {
+      let (found, u) = kmp_next(self.s, self.p, q, *c);
+      q = u;
+      if found {
+        r = Some(i + 1 - self.s.len());
+        break;
+      }
+    }
+    self.state = q;
+    self.pos = match r.as_ref() {
+      Some(i) => *i + self.s.len(),
+      None => self.t.len(),
+    };
+    r
+  }
+}
+
+impl<'a, T> KMPIterator<'a, T> {
+  pub fn matches_iterator(s: &'a [T], p: &'a Vec<usize>, t: &'a [T]) -> Self {
+    KMPIterator {
+      s,
+      p,
+      t,
+      state: 0,
+      pos: 0,
+    }
+  }
+}
--- rust/tests/string_functions.rs
+++ rust/tests/string_functions.rs
@@ -1,8 +1,21 @@
+use algo::string_functions;
 #[test]
 fn z_function() {
   let s = "abacaba";
   assert_eq!(
-    algo::string_functions::compute_z_function(s.as_bytes()),
+    string_functions::compute_z_function(s.as_bytes()),
     vec![0, 0, 1, 0, 3, 0, 1]
+  );
+}
+
+#[test]
+fn kmp() {
+  let w = "ABCDABD";
+  let s = "ABC ABCDAB ABCDABCDABDE";
+  let p = string_functions::compute_prefix_function(w.as_bytes());
+  assert_eq!(
+    string_functions::KMPIterator::matches_iterator(w.as_bytes(), &p, s.as_bytes())
+      .collect::<Vec<_>>(),
+    vec![15]
   );
 }
